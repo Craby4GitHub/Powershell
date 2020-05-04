695a50e0407de50e5fce1d041422d5510dfa4ed9
Craby4GitHub <wcrabtree@gmail.com>
2020-05-04 13:41:21 -0700

    dot sources WOL funtion

--- WOL.ps1
+++ WOL.ps1
@@ -1,47 +1,17 @@
 . "$PSScriptRoot\Write-Log.ps1"
-function Send-WOL
-{
-<# 
-  .SYNOPSIS  
-    Send a WOL packet to a broadcast address
-  .PARAMETER mac
-   The MAC address of the device that need to wake up
-  .PARAMETER ip
-   The IP address where the WOL packet will be sent to
-  .EXAMPLE 
-   Send-WOL -mac 00:11:32:21:2D:11 -ip 192.168.8.255 
-#>
+. "$PSScriptRoot\Send-WOL.ps1"
 
-[CmdletBinding()]
-param(
-[Parameter(Mandatory=$True,Position=1)]
-[string]$mac,
-$ip="255.255.255.255", 
-[int]$port=9
-)
-$broadcast = [Net.IPAddress]::Parse($ip)
- 
-$mac=(($mac.replace(":","")).replace("-","")).replace(".","")
-$target=0,2,4,6,8,10 | ForEach-Object {[convert]::ToByte($mac.substring($_,2),16)}
-$packet = (,[byte]255 * 6) + ($target * 16)
- 
-$UDPclient = new-Object System.Net.Sockets.UdpClient
-$UDPclient.Connect($broadcast,$port)
-[void]$UDPclient.Send($packet, 102) 
-
-}
 $Computer = Read-Host "Enter computer name:"
 
-$SiteCode = 'PCC'
-$SiteServer = 'do-sccm.pcc-domain.pima.edu'
 write-host "Looking for $Computer"
-$computerDetails=(Get-WmiObject -Class SMS_R_SYSTEM -Namespace "root\sms\site_$SiteCode" -computerName $SiteServer | Where-Object {$_.Name -eq "$Computer"})
+$computerDetails=(Get-WmiObject -Class SMS_R_SYSTEM -Namespace "root\sms\site_PCC" -computerName 'do-sccm.pcc-domain.pima.edu' | Where-Object {$_.Name -eq "$Computer"})
+
 if ($null -eq $computerDetails) {
     write-host "$Computer not found in SCCM"
     exit
 }
 
-$MAC = $computerDetails.MACAddresses[0]# | Out-String
+$MAC = $computerDetails.MACAddresses[0]
 $IP = $computerDetails.IPADDRESSES[0]
 write-host 'MAC:' $MAC
 write-host 'IP:' $IP
@@ -52,13 +22,15 @@ do {
     Send-WOL -mac $mac -ip $IP
     
     if (Test-Connection -ComputerName $Computer -Count 3 -quiet) {
-      Write-Log -status $true -Message "$Computer,$MAC,$IP" -logfile "$PSScriptRoot\log.csv"
-      write-host "im awake"
-      $i = 41
-    }else{
+        Write-Log -status $true -Message "$Computer,$MAC,$IP" -logfile "$PSScriptRoot\log.csv"
+        Write-host "im awake"
+    exit
+    }
+    else{
         Write-Log -status $false -Message "$Computer,$MAC,$IP" -logfile "$PSScriptRoot\log.csv"
         write-host 'nope'
     }
+
     Start-Sleep -Seconds 30
   $i++
 } until ($i -gt 4)
