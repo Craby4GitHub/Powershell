15bad2fd2c986b8c2bc9184472c978b4dc9cb566
Craby4GitHub <wcrabtree@gmail.com>
2020-06-24 15:43:36 -0700

    Dropdowns refer to csv file Once location is selected, selected equ is updated Added dropdown verification Replaced get-issues with get-file Replaced find-control with find-group Clear-fields now set fields, may maydate later

--- dental_ticket.ps1
+++ dental_ticket.ps1
@@ -25,6 +25,7 @@ $Form.ClientSize = "400,350"
 $Form.text = "Equipment Repair Form"
 $Form.TopMost = $true
 $Form.StartPosition = 'CenterScreen'
+$Form.Font = 'Microsoft Sans Serif,10'
 
 $ID_Num_Group = New-Object system.Windows.Forms.Groupbox
 $ID_Num_Group.height = 50
@@ -37,7 +38,6 @@ $ID_Num_Text.multiline = $false
 $ID_Num_Text.width = 100
 $ID_Num_Text.height = 20
 $ID_Num_Text.location = New-Object System.Drawing.Point(5, 17)
-$ID_Num_Text.Font = 'Microsoft Sans Serif,10'
 
 $Location_Group = New-Object system.Windows.Forms.Groupbox
 $Location_Group.height = 50
@@ -49,9 +49,9 @@ $Location_Dropdown              = New-Object system.Windows.Forms.ComboBox
 $Location_Dropdown.width        = 100
 $Location_Dropdown.height       = 20
 $Location_Dropdown.location     = New-Object System.Drawing.Point(5,17)
-$Location_Dropdown.Font         = 'Microsoft Sans Serif,10'
 $Location_Dropdown.Text = (Get-WmiObject -Class Win32_OperatingSystem).Description
-$Location_Dropdown.AutoCompleteMode = 'Suggest'
+$Location_Dropdown.DropDownStyle = "DropDown"
+$Location_Dropdown.AutoCompleteMode = 'SuggestAppend'
 $Location_Dropdown.AutoCompleteSource = 'ListItems'
 
 $Equipment_Group = New-Object system.Windows.Forms.Groupbox
@@ -64,8 +64,7 @@ $Equipment_Dropdown              = New-Object system.Windows.Forms.ComboBox
 $Equipment_Dropdown.width        = 100
 $Equipment_Dropdown.height       = 20
 $Equipment_Dropdown.location     = New-Object System.Drawing.Point(5,17)
-$Equipment_Dropdown.Font         = 'Microsoft Sans Serif,10'
-$Equipment_Dropdown.AutoCompleteMode = 'Suggest'
+$Equipment_Dropdown.AutoCompleteMode = 'SuggestAppend'
 $Equipment_Dropdown.AutoCompleteSource = 'ListItems'
 
 $Desc_Label = New-Object system.Windows.Forms.Label
@@ -74,14 +73,12 @@ $Desc_Label.AutoSize = $true
 $Desc_Label.width = 25
 $Desc_Label.height = 10
 $Desc_Label.location = New-Object System.Drawing.Point(140, 76)
-$Desc_Label.Font = 'Microsoft Sans Serif,10'
 
 $Desc_Text = New-Object system.Windows.Forms.TextBox
 $Desc_Text.multiline = $true
 $Desc_Text.width = 300
 $Desc_Text.height = 70
 $Desc_Text.location = New-Object System.Drawing.Point(50, 100)
-$Desc_Text.Font = 'Microsoft Sans Serif,10'
 
 $Issue_History_Label = New-Object system.Windows.Forms.Label
 $Issue_History_Label.text = "Current Issues"
@@ -89,11 +86,10 @@ $Issue_History_Label.AutoSize = $true
 $Issue_History_Label.width = 25
 $Issue_History_Label.height = 10
 $Issue_History_Label.location = New-Object System.Drawing.Point(140, 180)
-$Issue_History_Label.Font = 'Microsoft Sans Serif,10'
 
 $Issue_History = New-Object system.Windows.Forms.DataGridView
 $Issue_History.width = 360
-$Issue_History.height = 70
+$Issue_History.height = 100
 $Issue_History.location = New-Object System.Drawing.Point(20, 200)
 $Issue_History.ScrollBars = "Vertical"
 $Issue_History.AutoGenerateColumns = $true
@@ -101,31 +97,26 @@ $Issue_History.ColumnCount = 3
 $Issue_History.Columns[0].Name = 'Equipment'
 $Issue_History.Columns[1].Name = 'Issue'
 $Issue_History.Columns[2].Name = 'Submitted'
-
+$Issue_History.TabStop = $false
 
 $Submit_Button = New-Object system.Windows.Forms.Button
 $Submit_Button.text = "Submit"
 $Submit_Button.width = 60
 $Submit_Button.height = 30
 $Submit_Button.location = New-Object System.Drawing.Point(30, 310)
-$Submit_Button.Font = 'Microsoft Sans Serif,10'
-
-#not submitting, work on this
-$Form.AcceptButton = $Submit_Button  
 
 $Sumbit_Status = New-Object system.Windows.Forms.Label
 $Sumbit_Status.AutoSize = $true
 $Sumbit_Status.width = 250
 $Sumbit_Status.height = 10
 $Sumbit_Status.location = New-Object System.Drawing.Point(100, 310)
-$Sumbit_Status.Font = 'Microsoft Sans Serif,10'
+$Form.AcceptButton = $Submit_Button
 
 $Clear_Button = New-Object system.Windows.Forms.Button
 $Clear_Button.text = "Clear"
 $Clear_Button.width = 60
 $Clear_Button.height = 30
 $Clear_Button.location = New-Object System.Drawing.Point(300, 310)
-$Clear_Button.Font = 'Microsoft Sans Serif,10'
 
 $Form.controls.AddRange(@($ID_Num_Group, $Location_Group, $Equipment_Group, $Desc_Label, $Desc_Text, $Submit_Button, $Sumbit_Status, $Clear_Button, $Issue_History, $Issue_History_Label))
 $Equipment_Group.controls.AddRange(@($Equipment_Dropdown))
@@ -134,20 +125,20 @@ $Location_Group.controls.AddRange(@($Location_Dropdown))
 
 $Global:ErrorProvider = New-Object System.Windows.Forms.ErrorProvider
 
-function Get-Issues {   
+function Get-File($filePath) {   
     try {
-        $issues = Import-Csv -Path $IssuePath
+        $file = Import-Csv -Path $filePath
     }
     catch {
-        Export-Csv -InputObject $Submission -Path $IssuePath -NoTypeInformation
-        [System.Windows.Forms.MessageBox]::Show("Please relaunch. Error: " + $_.Exception.Message, 'Critical Issue', 'OK', 'Error')
+        [System.Windows.Forms.MessageBox]::Show("Error: " + $_.Exception.Message, 'Critical Issue', 'OK', 'Error')
         exit
     }
-    return $issues
+    return $file
 }
+
 function Update-CurrentIssues {
     $Issue_History.Rows.Clear()
-    foreach ($issue in Get-Issues) {
+    foreach ($issue in Get-File -filePath $IssuePath) {
         if (($OP_Text.Text -eq $issue.Operatory) -and ($issue.status -eq '')) {
             [void]$Issue_History.Rows.Add($($issue.'Equipment'), 
                                           $($issue.'Issue Description'),
@@ -157,35 +148,27 @@ function Update-CurrentIssues {
     # Sort the issues by most recent
     $Issue_History.Sort($Issue_History.Columns[2],[System.ComponentModel.ListSortDirection]::Descending)
 }
-
-function Find-Control($ControlType) {
-    $FormControls = @()
+function Find-Group() {
+    $FormGroups = @()
     ForEach ($control in $Form.Controls) {
         if ($control.ToString().StartsWith("System.Windows.Forms.GroupBox")) {
-            foreach ($gcontrol in $control.controls) {
-                if ($gcontrol.GetType().Name -eq $ControlType) {
-                    $FormControls += $gcontrol
-                }
-            }
-        }
-        elseif ($control.GetType().Name -eq $ControlType) {
-            $FormControls += $control
+            $FormGroups += $control
         }
     }
-    return $FormControls
+    return $FormGroups
 }
 
-function Clear-TextFields($Fields) {
-    foreach ($control in Find-Control -ControlType 'TextBox') {
-        $control.Clear()
-        $OP_Text.Text = (Get-WmiObject -Class Win32_OperatingSystem).description
-    }
+function Clear-Fields {
+    $ID_Num_Text.Text = ''
+    $Location_Dropdown.Text = (Get-WmiObject -Class Win32_OperatingSystem).description
+    $Equipment_Dropdown.Text = ''
+    $Desc_Text.Text = ''
 }
 
-function Confirm-NoTextError {
+function Confirm-NoError {
     $i = 0
-    foreach ($control in Find-Control -ControlType 'TextBox') {
-        if ($ErrorProvider.GetError($control).length -gt 0) {
+    foreach ($group in Find-Group) {
+        if ($ErrorProvider.GetError($group)) {
             $i++
         }
     }
@@ -195,6 +178,62 @@ function Confirm-NoTextError {
     else {
         return $true
     }
+}
+
+function Confirm-ID($CurrentField,$Group, $ErrorMSG) {
+    Switch -regex ($CurrentField.Text) {
+        #FACULTY
+        '^AJ((0[1-9])|(1[0-9])|20)$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^DAE[1-5]$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^DHE[1-5]$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^DR((0[1-9])|1[0-5])$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        #Student
+        '^DA(0[1-9]|((1|2)[0-9])|(30))$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^Y1((0[1-9])|((1|2)[0-9])|(30))$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^Y2((0[1-9])|((1|2)[0-9])|(30))$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        #ADMINISTRATIVE
+        '^ADM1$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^TECH$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^BA0[1-2]$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        '^FO0[1-2]$' {
+            $ErrorProvider.SetError($Group, '')
+            break
+        }
+        default {
+            $ErrorProvider.SetError($Group, $ErrorMSG)
+        }
+    }
+
 }
 
 function Confirm-UserInput($Regex, $CurrentField, $ErrorMSG) {
@@ -224,78 +263,43 @@ function Update-Submission {
     $Submission.TimeStamp = Get-Date
     return $Submission
 }
+function Confirm-Dropdown($Dropdown, $Group, $ErrorMSG) {
+    if ($Dropdown.Items -contains $Dropdown.Text) {
+        $ErrorProvider.SetError($Group,'')
+        Set-OULocation -Location $Dropdown.Text
+    }
+    else {
+        $ErrorProvider.SetError($Group, $ErrorMSG)
+    }     
+}
+
+# Generate ticket file
+# Export-Csv -InputObject $Submission -Path $IssuePath -NoTypeInformation
 #$IssuePath = "\\dentrix-prod-1\staff\front desk\tickets.csv"
 $IssuePath = "$PSScriptRoot\test.csv"
 Update-CurrentIssues
 
-$Clear_Button.Add_MouseUp( { Clear-TextFields })
-@('1','2') | ForEach-Object {[void] $Location_Dropdown.Items.Add($_)}
-@('xray','thingy') | ForEach-Object {[void] $Equipment_Dropdown.Items.Add($_)}
+$dentalArea = Get-File -filePath "$PSScriptRoot\Equipment and Locations.csv"
 
-function Confirm-ID($CurrentField,$ErrorMSG) {
-    Switch -regex ($CurrentField.Text) {
-        #FACULTY
-        '^AJ((0[1-9])|(1[0-9])|20)$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
+$Clear_Button.Add_MouseUp( { Clear-Fields })
+$dentalArea[0].PSObject.Properties.Name[1..$dentalArea[0].PSObject.Properties.Name.count] | ForEach-Object {[void] $Location_Dropdown.Items.Add($_)}
+$Location_Dropdown.Add_SelectedValueChanged({
+    $Equipment_Dropdown.Items.Clear()
+    $i=0
+    foreach ($equipment in $dentalArea.$($Location_Dropdown.SelectedItem)) {
+        if ($equipment -eq 'x') {
+            $Equipment_Dropdown.Items.Add($dentalArea[$i].'Equipment')
         }
-        '^DAE[1-5]$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^DHE[1-5]$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^DR((0[1-9])|1[0-5])$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        #Student
-        '^DA(0[1-9]|((1|2)[0-9])|(30))$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^Y1((0[1-9])|((1|2)[0-9])|(30))$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^Y2((0[1-9])|((1|2)[0-9])|(30))$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        #ADMINISTRATIVE
-        '^ADM1$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^TECH$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^BA0[1-2]$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        '^FO0[1-2]$' {
-            $ErrorProvider.SetError($CurrentField, '')
-            break
-        }
-        default {
-            $ErrorProvider.SetError($CurrentField, $ErrorMSG)
-        }
+        $i++
     }
-
-}
-
-
-$Submit_Button.Add_MouseUp( { Confirm-ID -CurrentField $ID_Num_Text -ErrorMSG 'INVALID STUDENT NUMBER' })
-$Submit_Button.Add_MouseUp( { Confirm-UserInput -regex "\w*" -CurrentField $Location_Dropdown -ErrorMSG 'INVALID LOCATION' })
-$Submit_Button.Add_MouseUp( { Confirm-UserInput -regex "." -CurrentField $Equipment_Dropdown -ErrorMSG 'INVALID EQUIPMENT' })
+})
+$Submit_Button.Add_MouseUp( { Confirm-ID -CurrentField $ID_Num_Text -Group $ID_Num_Group -ErrorMSG 'INVALID STUDENT NUMBER' })
+$Submit_Button.Add_MouseUp( { Confirm-Dropdown -Dropdown $Location_Dropdown -Group $Location_Group -ErrorMSG 'INVALID LOCATION' })
+$Submit_Button.Add_MouseUp( { Confirm-Dropdown -Dropdown $Equipment_Dropdown -Group $Equipment_Group -ErrorMSG 'INVALID EQUIPMENT' })
 $Submit_Button.Add_MouseUp( { Confirm-UserInput -regex "." -CurrentField $Desc_Text -ErrorMSG 'INVALID DESCRIPTION' })
 $Submit_Button.Add_MouseUp( {
-        if (Confirm-NoTextError) {
-            $error.Clear()
+        if (Confirm-NoError) {
+            $ErrorProvider.Clear()
             try {
                 Update-Submission | Export-Csv -path $IssuePath -append -NoTypeInformation -force
                 Update-CurrentIssues
