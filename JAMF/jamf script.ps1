5f07985f904354b0af90c1703418421b884d7736
Craby4GitHub <github@willcrabtree.com>
2020-12-19 16:57:26 -0700

    Creating script to move mobile devies prestage

--- JAMF/jamf script.ps1
+++ JAMF/jamf script.ps1
@@ -1,3 +1,10 @@
-. (Join-Path $PSSCRIPTROOT "Get-JamfComputers.ps1")
-$computers = Get-JamfComputers
-write-host $computers
+. (Join-Path $PSSCRIPTROOT "JAMF-API.ps1")
+
+$SerialNumbers = @('C07VX4EYHNM4', 'CCQNF17VG22V', 'DMPCTNH2MF3V', 'DMPWLX54JF8M')
+
+$JAMFMobileDevices = Get-JamfMobileDevicePreStageScope
+foreach ($SerialNumber in $SerialNumbers) {
+    if (($JAMFMobileDevices | Select-Object -ExpandProperty $SerialNumber) -eq $PrestageID) {
+        
+    } 
+}
