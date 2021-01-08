3c73a8c05404f4064c5adb24a85300b146187da1
Craby4GitHub <github@willcrabtree.com>
2021-01-08 13:10:18 -0700

    cleanup

--- Printer Add.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Printer = Read-Host "Please list printers seperated by a comma"
-
-##Map Printer
-foreach($Printer in $Printers){
-	RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /z /n\\EC-Print\$Printer
-	}
