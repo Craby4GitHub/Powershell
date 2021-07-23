3b69d083547daf615eb09d398a1471b6a2fdd5b4
Craby4GitHub <github@willcrabtree.com>
2021-07-23 16:59:59 -0700

    Started to add AD functionality Added login for AD creds Added AD tree functionality

--- OSDComputerNameandOU.ps1
+++ OSDComputerNameandOU.ps1
@@ -59,11 +59,11 @@ function OSD-GUI {
     $ComputerName_Label = New-Object system.Windows.Forms.Label
     $ComputerName_Label.Text = 'Create Computer Name'
     $ComputerName_Label.Font = 'Segoe UI, 10pt,style=bold'
-    $ComputerName_Label.Dock = 'Bottom'
+    $ComputerName_Label.Dock = 'Fill'
     $ComputerName_Label.Anchor = 'Bottom'
     $ComputerName_Label.AutoSize = $true
 
-    $CampusList = @(('29', 'Adult Education'), ('ER', 'Adult Education'), ('EP', 'Adult Education'), ('DV', 'Desert Vista'), ('DO', 'District'), ('DC', 'Downtown'), ('EC', 'East'), ('MS', 'Maintenance and Security'), ('NW', 'Northwest'), ('WC', 'West'), ('PCC', 'West'))
+    $CampusList = @(('29', '29th St.'), ('ER', 'El Rio'), ('EP', 'El Pueblo'), ('DV', 'Desert Vista'), ('DO', 'District'), ('DC', 'Downtown'), ('DM', 'DM'), ('EC', 'East'), ('MS', 'Maintenance and Security'), ('NW', 'Northwest'), ('WC', 'West'), ('PCC', 'West'))
 
     $ComputerName_Campus_Label = New-Object system.Windows.Forms.Label
     $ComputerName_Campus_Label.Text = 'Campus'
@@ -139,6 +139,7 @@ function OSD-GUI {
     $EDU_RadioButton.AutoSize = $true
     $EDU_RadioButton.CheckAlign = 'MiddleRight'
     $EDU_RadioButton.TextAlign = 'MiddleRight'
+    $EDU_RadioButton.Checked = $true
 
     $PCC_RadioButton = New-Object System.Windows.Forms.RadioButton
     $PCC_RadioButton.Text = 'PCC'
@@ -155,7 +156,79 @@ function OSD-GUI {
     $Submit_Button.AutoSize = $true
     $Form.AcceptButton = $Submit_Button
 
+    $treeView = New-Object System.Windows.Forms.TreeView
+    $treeView.Dock = 'Fill'
+
     $Form.controls.Add($Main_LayoutPanel)
+    $Form.controls.Add($treeView)
+
+    #region Login Window
+    $Login_Form = New-Object system.Windows.Forms.Form
+    $Login_Form.FormBorderStyle = "FixedDialog"
+    $Login_Form.ClientSize = "400,220"
+    $Login_Form.TopMost = $true
+    $Login_Form.StartPosition = 'CenterScreen'
+    $Login_Form.ControlBox = $false
+    $Login_Form.AutoSize = $true
+
+    $Username_TextBox = New-Object system.Windows.Forms.TextBox
+    $Username_TextBox.multiline = $false
+    $Username_TextBox.Text = 'Username'
+    $Username_TextBox.TabIndex = 1
+
+    $Password_TextBox = New-Object system.Windows.Forms.TextBox
+    $Password_TextBox.multiline = $false
+    $Password_TextBox.Text = "PimaRocks"
+    $Password_TextBox.TabIndex = 2
+    $Password_TextBox.PasswordChar = '*'
+    $Password_TextBox.Select()
+
+    $OK_Button_Login = New-Object system.Windows.Forms.Button
+    $OK_Button_Login.Text = "Login"
+    $OK_Button_Login.Dock = 'Fill'
+    $OK_Button_Login.TabIndex = 3
+    $OK_Button_Login.FlatStyle = 1
+    $OK_Button_Login.FlatAppearance.BorderSize = 0
+    $Login_Form.AcceptButton = $OK_Button_Login
+    $Login_Form.AcceptButton.DialogResult = 'OK'
+
+    $Cancel_Button_Login = New-Object system.Windows.Forms.Button
+    $Cancel_Button_Login.Text = "Cancel"
+    $Cancel_Button_Login.TabIndex = 4
+    $Cancel_Button_Login.Dock = 'Fill'
+
+    $Login_Form.CancelButton = $Cancel_Button_Login
+    $Login_Form.CancelButton.DialogResult = 'Cancel'
+
+    $LayoutPanel_Login = New-Object System.Windows.Forms.TableLayoutPanel
+    $LayoutPanel_Login.Dock = "Fill"
+    $LayoutPanel_Login.ColumnCount = 4
+    $LayoutPanel_Login.RowCount = 5
+    #$LayoutPanel_Login.CellBorderStyle = 1
+    [void]$LayoutPanel_Login.ColumnStyles.Add((new-object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 1)))
+    [void]$LayoutPanel_Login.ColumnStyles.Add((new-object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 10)))
+    [void]$LayoutPanel_Login.ColumnStyles.Add((new-object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 10)))
+    [void]$LayoutPanel_Login.ColumnStyles.Add((new-object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 1)))
+
+    [void]$LayoutPanel_Login.RowStyles.Add((new-object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 60)))
+    [void]$LayoutPanel_Login.RowStyles.Add((new-object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 60)))
+    [void]$LayoutPanel_Login.RowStyles.Add((new-object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 60)))
+    [void]$LayoutPanel_Login.RowStyles.Add((new-object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 60)))
+    [void]$LayoutPanel_Login.RowStyles.Add((new-object System.Windows.Forms.RowStyle([System.Windows.Forms.SizeType]::Percent, 10)))
+
+    $LayoutPanel_Login.Controls.Add($Username_TextBox, 1, 0)
+    $LayoutPanel_Login.SetColumnSpan($Username_TextBox, 2)
+    $LayoutPanel_Login.Controls.Add($Password_TextBox, 1, 1)
+    $LayoutPanel_Login.SetColumnSpan($Password_TextBox, 2)
+    $LayoutPanel_Login.Controls.Add($Domain_LayoutPanel, 1, 2)
+    $LayoutPanel_Login.SetColumnSpan($Domain_LayoutPanel, 2)
+    $LayoutPanel_Login.Controls.Add($OK_Button_Login, 1, 3)
+    $LayoutPanel_Login.Controls.Add($Cancel_Button_Login, 2, 3)
+    $Login_Form.controls.Add($LayoutPanel_Login)
+    #EndRegion
+
+
+
     #region UI Layout
 
     #region Main Layout
@@ -163,15 +236,15 @@ function OSD-GUI {
     $Main_LayoutPanel.Controls.Add($ComputerName_LayoutPanel, 0, 0)
     $Main_LayoutPanel.SetColumnSpan($ComputerName_LayoutPanel, 3)
 
-    $Main_LayoutPanel.Controls.Add($Domain_LayoutPanel, 0, 1)
-    $Main_LayoutPanel.SetColumnSpan($Domain_LayoutPanel, 3)
+    $Main_LayoutPanel.Controls.Add($treeView, 0, 1)
+    $Main_LayoutPanel.SetColumnSpan($treeView, 3)
 
     $Main_LayoutPanel.Controls.Add($Submit_Button, 1, 2)
     #endregion
 
     #region Computer Name
-    $ComputerName_LayoutPanel.Controls.Add($ComputerName_Label, 1, 0)
-    $ComputerName_LayoutPanel.SetColumnSpan($ComputerName_Label, 2)
+    $ComputerName_LayoutPanel.Controls.Add($ComputerName_Label, 0, 0)
+    $ComputerName_LayoutPanel.SetColumnSpan($ComputerName_Label, 4)
     $ComputerName_LayoutPanel.Controls.Add($ComputerName_Campus_Label, 0, 1)
     $ComputerName_LayoutPanel.Controls.Add($ComputerName_Campus_Dropdown, 0, 2)
 
@@ -203,15 +276,35 @@ function OSD-GUI {
             return $true
         }
     }
+
+    Function AddNodes ( $Node, $CurrentOU, $Domain ) {
+        $NodeSub = $Node.Nodes.Add($CurrentOU.DistinguishedName.toString(), $CurrentOU.Name)
+        Get-ADOrganizationalUnit -Filter * -SearchScope OneLevel -SearchBase $CurrentOU -Server "$($Domain)-domain.pima.edu" | ForEach-Object { AddNodes $NodeSub $_ }
+    }
     #endregion
 
     #region Actions
+    $ComputerName_Campus_Dropdown.Add_SelectedIndexChanged( {
+            $treeView.Nodes.Clear()
+            if ($EDU_RadioButton.Checked -eq $true) {
+                Get-ADOrganizationalUnit -Filter * -SearchScope OneLevel -SearchBase "OU=EDU_Computers,DC=edu-domain,DC=pima,DC=edu" -Server 'edu-domain.pima.edu' | ForEach-Object { AddNodes $treeView $_ -Domain 'edu' }
+            }
+            elseif ($PCC_RadioButton.Checked -eq $true) {
+                if ($ComputerName_Campus_Dropdown.Items -contains $ComputerName_Campus_Dropdown.Text) {
+                    Get-ADOrganizationalUnit -Filter * -SearchScope OneLevel -SearchBase "OU=$($CampusList[$ComputerName_Campus_Dropdown.SelectedIndex][1]),OU=PCC,DC=PCC-Domain,DC=pima,DC=edu" -Server 'pcc-domain.pima.edu' | ForEach-Object { AddNodes $treeView $_ 'pcc' }
+                }
+                else {
+                    Get-ADOrganizationalUnit -Filter * -SearchScope OneLevel -SearchBase "OU=PCC,DC=PCC-Domain,DC=pima,DC=edu" -Server 'pcc-domain.pima.edu' | ForEach-Object { AddNodes $treeView $_ 'pcc' }
+                }
+            }  
+        })
 
     $PCCNumber = (Get-WmiObject -Query "Select * from Win32_SystemEnclosure").SMBiosAssetTag
     if ($PCCNumber -match '^\d{6}$') {
+        $ComputerName_PCCNumber_Textbox.ReadOnly = $true
         $ComputerName_PCCNumber_Textbox.Text = $PCCNumber
     }
-
+ 
     $Submit_Button.Add_Click( { 
             if ($ComputerName_Campus_Dropdown.Items -contains $ComputerName_Campus_Dropdown.Text) {
                 $ErrorProvider.SetError($ComputerName_Label, '')
@@ -250,22 +343,9 @@ function OSD-GUI {
                 $ErrorProvider.SetError($ComputerName_Label, 'Select a proper campus')
             }            
         
-            # Verify Domain
-            $ErrorProvider.SetError($DomainSelection_Label, '')
-            if ($EDU_RadioButton.Checked -eq $true) {
-                $Domain = 'EDU-Domain.pima.edu'
-                $OULocation = "LDAP://OU=$($CampusList[$ComputerName_Campus_Dropdown.SelectedIndex][1]),OU=Staging,DC=$Domain,DC=pima,DC=edu"
-            }
-            if ($PCC_RadioButton.Checked -eq $true) {
-                $Domain = 'PCC-Domain.pima.edu'
-                $OULocation = "LDAP://OU=$($CampusList[$ComputerName_Campus_Dropdown.SelectedIndex][1]),OU=Staging,DC=$Domain,DC=pima,DC=edu"
-            }
-            elseif (($EDU_RadioButton.Checked -or $PCC_RadioButton.Checked) -eq $false) {
-                $ErrorProvider.SetError($DomainSelection_Label, 'Select a domain')
-            } 
-        
             if (Confirm-NoError) {
                 # Output for testing
+                $OULocation = "LDAP://$($treeView.SelectedNode.Name)"
                 $ComputerName.ToUpper(), $OULocation, 'Passed' | Out-File '.\OSDTest.csv' -Append
 
                 #Enable when deployed
@@ -280,22 +360,53 @@ function OSD-GUI {
     #endregion
 
     # Test Values
-    $ComputerName_Campus_Dropdown.SelectedItem = $Campus
+    #$ComputerName_Campus_Dropdown.SelectedItem = $Campus
     $ComputerName_BuildingRoom_Textbox.Text = $Bldg
     $ComputerName_PCCNumber_Textbox.Text = $PCC
     $ComputerName_Suffix_Textbox.Text = $Suffix
-    switch -regex ($Domain) {
-        'PCC' { $PCC_RadioButton.Checked = $true }
-        'EDU' { $EDU_RadioButton.Checked = $true }
-        Default {}
+
+    [void]$Login_Form.ShowDialog()
+    if ($Login_Form.DialogResult -eq 'OK') {
+        $Password = ConvertTo-SecureString $Password_TextBox.Text -AsPlainText -Force
+        $global:Credentials = New-Object System.Management.Automation.PSCredential ($Username_TextBox.text, $Password)
+
+        try {
+            $ADDomain = Get-ADDomain -Credential $Credentials
+        }
+        catch [System.Security.Authentication.AuthenticationException] {
+            write-host "incorrect login"
+        }
+        catch {
+            write-host 'hm...'
+        }
+
+        if ($ADDomain.Name -match $PCC_RadioButton.text -or $ADDomain.Name -match $EDU_RadioButton.text) {
+            [void]$Form.ShowDialog()
+            break
+        }
+        else {
+            $RelogChoice = [System.Windows.Forms.MessageBox]::Show("Login Failed, please relaunch.", 'Warning', 'RetryCancel', 'Warning')
+            switch ($RelogChoice) {
+                # Need to figure out logic to re-enter creds and what not
+                'Retry' { 
+                    # Retry is not working
+                    [void]$Login_Form.ShowDialog() 
+                    break
+                }
+                'Cancel' {
+                    #Write-Log -Message "$($Credentials.UserName) failed to login" -Level WARN
+                    #exit
+                }
+            }        
+        }
     }
-    [void]$Form.ShowDialog()
-    $Submit_Button.PerformClick()
+    elseif ($Login_Form.DialogResult -eq 'Cancel') {
+    }
 }
 
 $CampusShortList = @('29', 'ER', 'EP', 'DV', 'DO', 'DC', 'EC', 'MS', 'NW', 'WC', 'PCC')
 $RandomRooms = @('CG11', 'E513', 'AH321', 'emp', 'stu')
 
 for ($i = 0; $i -lt 1; $i++) {
-    OSD-GUI -Campus $(get-random -InputObject $CampusShortList) -Bldg $(get-random -InputObject $RandomRooms) -PCC $(get-random -Maximum 999999) -Suffix $( -join ((65..90) + (97..121) | Get-Random -Count 2 | % { [char]$_ })) -Domain $(get-random -InputObject 'EDU', 'PCC')
+    OSD-GUI -Campus $(get-random -InputObject $CampusShortList) -Bldg $(get-random -InputObject $RandomRooms) -PCC $(get-random -Maximum 999999 -Minimum 111111) -Suffix $( -join ((65..90) + (97..121) | Get-Random -Count 2 | % { [char]$_ })) -Domain $(get-random -InputObject 'EDU', 'PCC')
 }
