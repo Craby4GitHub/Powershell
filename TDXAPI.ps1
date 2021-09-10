2351fea96a6f337d9727b6ed80fea51e2585e95c
Craby4GitHub <github@willcrabtree.com>
2021-09-10 16:45:57 -0700

    Built up Edit-Asset. Added Try-catch for atuh. Stole GetRateLimitWaitPeriodMs.

--- TDXAPI.ps1
+++ TDXAPI.ps1
@@ -6,11 +6,37 @@ function ApiAuthenticateAndBuildAuthHeaders {
         WebServicesKey = $apiWSKey;
     } | ConvertTo-Json
 
-    $authToken = Invoke-RestMethod -Method Post -Uri $authUri -Body $authBody -ContentType "application/json"
-    
+    $authToken = try {
+        Invoke-RestMethod -Method Post -Uri $authUri -Body $authBody -ContentType "application/json"
+    }
+    catch {
+        # Display errors and exit script.
+        Write-host "API authentication failed:"
+        Exit(1)
+    }
+
     $apiHeadersInternal = @{"Authorization" = "Bearer " + $authToken }
 
     return $apiHeadersInternal
+}
+
+function GetRateLimitWaitPeriodMs {
+    param (
+        $apiCallResponse
+    )
+
+    # Get the rate limit period reset.
+    # Be sure to convert the reset date back to universal time because PS conversions will go to machine local.
+    $rateLimitReset = ([DateTime]$apiCallResponse.Headers["X-RateLimit-Reset"]).ToUniversalTime()
+
+    # Calculate the actual rate limit period in milliseconds.
+    # Add 5 seconds to the period for clock skew just to be safe.
+    $duration = New-TimeSpan -Start ((Get-Date).ToUniversalTime()) -End $rateLimitReset
+    $rateLimitMsPeriod = $duration.TotalMilliseconds + 5000
+
+    # Return the millisecond rate limit wait.    
+    return $rateLimitMsPeriod
+
 }
 
 function Search-Asset($serialNumber) {
@@ -23,7 +49,43 @@ function Search-Asset($serialNumber) {
         SerialLike = $serialNumber;
     } | ConvertTo-Json
 
-    $resp = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $uri -Body $assetBody -ContentType "application/json" -UseBasicParsing
+
+    try {
+
+        $resp = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $uri -Body $assetBody -ContentType "application/json" -UseBasicParsing
+        return $resp
+
+    }
+    catch {
+        # If we got rate limited, try again after waiting for the reset period to pass.
+        $statusCode = $_.Exception.Response.StatusCode.value__
+        if ($statusCode -eq 429) {
+
+            # Get the amount of time we need to wait to retry in milliseconds.
+            $resetWaitInMs = GetRateLimitWaitPeriodMs -apiCallResponse $_.Exception.Response
+            Write-host "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
+
+            # Wait to retry now.
+            Start-Sleep -Milliseconds $resetWaitInMs
+
+            # Now retry.
+            Write-host "Retrying API call to retrieve all location custom attribute data for the organization."
+            Search-Asset -serialNumber $serialNumber
+        }
+        else {
+            # Display errors and exit script.
+
+            Exit(1)
+        
+        }
+    }
+}
+
+function Get-Asset($ID) {
+    
+    $uri = $apiBaseUri + "api/$($appID)/assets/$($ID)"
+
+    $resp = Invoke-RestMethod -Method GET -Headers $apiHeaders -Uri $uri  -ContentType "application/json" -UseBasicParsing
     return $resp
 }
 
@@ -35,7 +97,7 @@ function Get-AllAssetStatus {
     $resp = $status = Invoke-RestMethod -Method GET -Headers $apiHeaders -Uri $uri -ContentType "application/json" -UseBasicParsing
     foreach ($status in $resp) {
         if ($status.IsActive) {
-            $statuses += @($status.Name,$status.ID)
+            $statuses += @($status.Name, $status.ID)
         }
     }
     return $statuses
@@ -44,84 +106,88 @@ function Get-AllAssetStatus {
 function Edit-Asset {
     param (
         [Parameter(Mandatory = $true)]
-        [int]$ID,
-        [Parameter(Mandatory = $true)]
-        [string]$SerialNumber,
-        [Parameter(Mandatory = $true)]
-        $StatusID,
-        $OwningCustomerID
+        [object]$Asset
     )
+    $allAttributes = @()
 
+    $assetAttributes = (get-asset -ID $asset.ID).Attributes
+    foreach ($attribute in $assetAttributes) {
+        if ($attribute.ID -ne '126172') {
+            $allAttributes += [PSCustomObject]@{
+                ID    = $attribute.ID;
+                Value = $attribute.Value;
+            }
+        }        
+    }
 
-<#
-
-Changed Status from "Active" to "Disposed".
-Changed Supplier from "HP" to "".
-Changed Product Model from "HP - Z230 TWR" to "".
-Changed Location from "West Campus" to "Nothing".
-Changed Owner from "Will Crabtree" to "Aakash Gupta".
-Changed Service Tag from "140759" to "".
-Changed Acquisition Date from "Wed 5/21/14" to "".
-Changed Last Inventory Date from "03/19/2021" to "Nothing".
-Changed PO # from "P1421343" to "Nothing".
-Changed Role from "Employee Device" to "Nothing".
-Changed Warranty End Date from "05/30/2017" to "Nothing".
-
-
-#>
-
-
-
-
-
-
+    $SCCM = Get-SCCMDevice -computerName $('*' + $Asset.Tag + '*')
+    if ($SCCM.LastHardwareScan -ne $null) {
+        #Pull SCCM Last Hardware scan to TDX Last Inventory Date
+        $allAttributes += [PSCustomObject]@{
+            ID    = "126172";
+            Value = $SCCM.LastHardwareScan.ToString("o");
+        }
+    }
+    
     $assetBody = [PSCustomObject]@{
-        SerialNumber = $SerialNumber;
-        StatusID = $StatusID;
-        OwningCustomerID = $OwningCustomerID;
+        FormID                  = $Asset.FormID;
+        ProductModelID          = $Asset.ProductModelID;
+        SupplierID              = $Asset.SupplierID;
+        StatusID                = $Asset.StatusID;
+        LocationID              = $Asset.LocationID;
+        LocationRoomID          = $Asset.LocationRoomID;
+        Tag                     = $Asset.Tag;
+        SerialNumber            = $Asset.SerialNumber;
+        Name                    = $Asset.Name;
+        PurchaseCost            = $Asset.PurchaseCost;
+        AcquisitionDate         = $Asset.AcquisitionDate;
+        ExpectedReplacementDate = $Asset.ExpectedReplacementDate;
+        RequestingCustomerID    = $Asset.RequestingCustomerID;
+        RequestingDepartmentID  = $Asset.RequestingDepartmentID;
+        OwningCustomerID        = $Asset.OwningCustomerID;
+        OwningDepartmentID      = $Asset.OwningDepartmentID;
+        ParentID                = $Asset.ParentID;
+        MaintenanceScheduleID   = $Asset.MaintenanceScheduleID;
+        ExternalID              = $Asset.ExternalID;
+        ExternalSourceID        = $Asset.ExternalSourceID;
+        Attributes              = @($allAttributes);
     } | ConvertTo-Json
-
-    $getAssetSearchUri = $apiBaseUri + "api/$($appID)/assets/$($ID)"
-    $resp = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $getAssetSearchUri -Body $assetBody -ContentType "application/json" -UseBasicParsing
-
+   
+    
+    $Uri = $apiBaseUri + "api/$($appID)/assets/$($Asset.ID)"
+    $resp = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $uri -Body $assetBody -ContentType "application/json" -UseBasicParsing
+    return $resp
 }
 
-function SCCmCrap($computernName) {
+function Get-SCCMDevice($computerName) {
 
-    Get-CMDevice -Name $computerName -Fast
+    # Site configuration
+    $SiteCode = "PCC" # Site code 
+    $ProviderMachineName = "do-sccm.pcc-domain.pima.edu" # SMS Provider machine name
 
-    <#
-    
-    LastActiveTime                           : 9/1/2021 10:38:51 PM
-    LastClientCheckTime                      : 8/15/2021 10:10:11 AM
-    LastDDR                                  : 8/31/2021 3:29:56 PM
-    LastHardwareScan                         : 8/31/2021 4:15:34 PM
-    LastLogonUser                            : wrcrabtree
-    last check in and internal ip
-    #>
+    $initParams = @{}
+  
+    # Import the ConfigurationManager.psd1 module 
+    if ($null -eq (Get-Module ConfigurationManager)) {
+        Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+    }
+
+    # Connect to the site's drive if it is not already present
+    if ($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
+        New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+    }
+
+    # Set the current location to be the site code.
+    Set-Location "$($SiteCode):\" @initParams
+
+
+    $device = Get-CMDevice -Name $computerName -Fast
+
+    return $device
 }
 
 $TDXCreds = Get-Content $PSScriptRoot\TDXCreds.json | ConvertFrom-Json
 
-# Site configuration
-$SiteCode = "PCC" # Site code 
-$ProviderMachineName = "do-sccm.pcc-domain.pima.edu" # SMS Provider machine name
-
-$initParams = @{}
-  
-
-# Import the ConfigurationManager.psd1 module 
-if ($null -eq (Get-Module ConfigurationManager)) {
-    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
-}
-
-# Connect to the site's drive if it is not already present
-if ($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
-    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
-}
-
-# Set the current location to be the site code.
-Set-Location "$($SiteCode):\" @initParams
 
 $apiBaseUri = 'https://service.pima.edu/SBTDWebApi/'
 
@@ -131,6 +197,9 @@ $apiWSKey = $TDXCreds.Key
 $appID = 1258
 $global:apiHeaders = ApiAuthenticateAndBuildAuthHeaders
 
-$asset = Search-Asset -serialNumber 140759
+$asset = Search-Asset -serialNumber MXL824145Z
 
-Get-AllAssetStatus
+
+Edit-Asset -Asset $asset
+
+#Get-AllAssetStatus
