76fe9ca9e159551473644fc7b6fd2bcf1769046f
Craby4GitHub <github@willcrabtree.com>
2021-09-13 17:53:26 -0700

    Added loop for all TDX Assets. Added SCCM Serial Number Lookup.

--- TDX/Update-TDXAssetInventoryDate.ps1
+++ TDX/Update-TDXAssetInventoryDate.ps1
@@ -28,8 +28,18 @@ function Get-SCCMDevice($computerName) {
 . (Join-Path $PSSCRIPTROOT "TDX-API.ps1")
 #. Join-Path (Get-ChildItem .\JAMF) "JAMF-API.ps1"
 
-$tdxAsset = Search-TDXAsset -serialNumber MXL824145Z
+#$allSCCMDevices = Get-CMDevice -CollectionName 'Agent Installed' | Select-Object Name, ResourceID, LastHardwareScan
+$allTDXAssets = Search-TDXAsset
+foreach ($tdxAsset in $allTDXAssets) {
+    $SCCM = Get-SCCMDevice -computerName $('*' + $tdxAsset.Tag + '*')
+    $sccmSerialNumber = (Get-WmiObject -Class SMS_G_system_SYSTEM_ENCLOSURE  -Namespace root\sms\site_PCC -ComputerName "do-sccm.pcc-domain.pima.edu" -Filter "ResourceID = $($SCCM.ResourceID)").Serialnumber
 
-$SCCM = Get-SCCMDevice -computerName $('*' + $tdxAsset.Tag + '*')
+    if($tdxAsset.SerialNumber -eq $sccmSerialNumber){
+    #need to add check if the invetory date is older than what we are updating
+    $assetAttributes = (Get-TDXAssetDetails -ID $asset.ID).Attributes
+        
+        write-host "Found $($tdxAsset.tag)" -ForegroundColor Green
+        Edit-TDXAsset -Asset $tdxAsset -sccmLastHardwareScan $SCCM.LastHardwareScan
+    }
 
-Edit-TDXAsset -Asset $tdxAsset -sccmLastHardwareScan $SCCM.LastHardwareScan
+}
