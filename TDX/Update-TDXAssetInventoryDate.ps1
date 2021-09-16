cdc314bbf0284a97351254300a0bf874a1d0f28b
Craby4GitHub <github@willcrabtree.com>
2021-09-15 17:05:49 -0700

    Sccm query to psobject to reduce quota limit.

--- TDX/Update-TDXAssetInventoryDate.ps1
+++ TDX/Update-TDXAssetInventoryDate.ps1
@@ -28,29 +28,37 @@ function Get-SCCMDevice($computerName) {
 . (Join-Path $PSSCRIPTROOT "TDX-API.ps1")
 #. Join-Path (Get-ChildItem .\JAMF) "JAMF-API.ps1"
 
-#$allSCCMDevices = Get-CMDevice -CollectionName 'Agent Installed' | Select-Object Name, ResourceID, LastHardwareScan
+$sccmquery = Get-CMDevice -CollectionName 'Agent Installed' | Select-Object Name, ResourceID, LastDDR
+
+# Converting from SCCM object otherwise we will hit quota limit when doing sccm searches
+$allSCCMDevices = [System.Collections.ArrayList]@()
+foreach ($sccmDevice in $sccmquery) {
+    $allSCCMDevices += [PSCustomObject]@{
+        Name       = $sccmDevice.Name
+        ResourceID = $sccmDevice.ResourceID
+        LastDDR    = $sccmDevice.LastDDR
+    }
+}
+
 $allTDXAssets = Search-TDXAsset
-
-
-
 foreach ($tdxAsset in $allTDXAssets) {
     write-host "Searching $($tdxasset.tag) in SCCM"
-    $SCCM = Get-SCCMDevice -computerName $('*' + $tdxAsset.Tag + '*')
+    #$SCCM = Get-SCCMDevice -computerName $('*' + $tdxAsset.Tag + '*')
+    $SCCM = $allSCCMDevices | Where-Object -Property name -like "*$($tdxAsset.Tag)*"
 
     # Fix logic if more than one entry. Will get generic error if not
     $sccmSerialNumber = (Get-WmiObject -Class SMS_G_system_SYSTEM_ENCLOSURE  -Namespace root\sms\site_PCC -ComputerName "do-sccm.pcc-domain.pima.edu" -Filter "ResourceID = $($SCCM.ResourceID)").Serialnumber
 
     # Verify the assets serial number match. Reason: Computer could be misnamed or there could be virtual machines as we only search SCCM on pcc number
-    write-host "Verify: TDX SN: $($tdxasset.SerialNumber)-----SCCM SN:$sccmSerialNumber"
     if ($tdxAsset.SerialNumber -eq $sccmSerialNumber) {
+        write-host "Serial Numbers match" -ForegroundColor Green
         $assetAttributes = (Get-TDXAssetDetails -ID $tdxAsset.ID).Attributes
 
         # Check to see if TDX inventory date is newer than the last SCCM heartbeat
         # Wishlist: I would like to move away from using an index value on the attributes as more attributes may be added in the future
-        write-host "Verify: TDX Date: $($assetAttributes[1].value)-----SCCM Date:$($SCCM.LastDDR)"
-        if ($assetAttributes[1].value -gt $SCCM.LastDDR) {
-            write-host "Found $($tdxAsset.tag)" -ForegroundColor Green
-            Edit-TDXAsset -Asset $tdxAsset -sccmLastHardwareScan $SCCM.LastHardwareScan
+        if ($assetAttributes[1].value -lt $SCCM.LastDDR) {
+            write-host "Updating $($tdxAsset.tag) inventory date to $($SCCM.LastDDR) " -ForegroundColor Green
+            Edit-TDXAsset -Asset $tdxAsset -sccmLastHardwareScan $SCCM.LastDDR
         }
     }
 }
