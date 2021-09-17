fb72e08299592ff02687bc61c2d7cd85e2e91902
Craby4GitHub <github@willcrabtree.com>
2021-09-17 16:58:05 -0700

    Added stolen write-log function. Added write-log to api calls. Updated Get-TDXAssetAttributes

--- TDX/TDX-API.ps1
+++ TDX/TDX-API.ps1
@@ -1,3 +1,83 @@
+# Global Variables
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$scriptName = ($MyInvocation.MyCommand.Name -split '\.')[0]
+$logFile = "$scriptPath\$scriptName.log"
+
+#region 
+function Write-Log {
+    
+    param (
+        [ValidateSet('ERROR', 'INFO', 'VERBOSE', 'WARN')]
+        [Parameter(Mandatory = $true)]
+        [string]$level,
+
+        [Parameter(Mandatory = $true)]
+        [string]$string
+    )
+    
+    # First roll log if over 10MB.
+    if ($logFile | Test-Path) {
+		
+        # Get the full info for the current log file.
+        $currentLog = Get-Item $logFile
+		
+        # Get log size in MB (from B)
+        $currentLogLength = ([double]$currentLog.Length / 1024.00 / 1024.00)
+		
+        # If the log file exceeds 10mb, roll it by renaming with a timestamped name.
+        if ($currentLogLength -ge 10) {
+			
+            $newLogFileName = (Get-Date).ToString("yyyy-MM-dd HHmmssfff") + " " + $currentLog.Name
+            Rename-Item -Path $currentLog.FullName -NewName $newLogFileName -Force
+        }
+    }
+	
+    # Next remove old log files if there are more than 10.
+    Get-ChildItem -Path $scriptPath -Filter *.log* | `
+        Sort CreationTime -Desc | `
+        Select -Skip 10 | `
+        Remove-Item -Force
+
+    $logString = (Get-Date).toString("yyyy-MM-dd HH:mm:ss") + " [$level] $string"
+    #Add-Content -Path $logFile -Value $logString -Force
+    [System.IO.File]::AppendAllText($logFile, $logString + "`r`n")
+	
+    $foregroundColor = $host.ui.RawUI.ForegroundColor
+    $backgroundColor = $host.ui.RawUI.BackgroundColor
+
+    Switch ($level) {
+        { $_ -eq 'VERBOSE' -or $_ -eq 'INFO' } {
+            
+            Out-Host -InputObject "$logString"
+            
+        }
+
+        { $_ -eq 'ERROR' } {
+
+            $host.ui.RawUI.ForegroundColor = "Red"
+            $host.ui.RawUI.BackgroundColor = "Black"
+
+            Out-Host -InputObject "$logString"
+    
+            $host.ui.RawUI.ForegroundColor = $foregroundColor
+            $host.UI.RawUI.BackgroundColor = $backgroundColor
+        }
+
+        { $_ -eq 'WARN' } {
+    
+            $host.ui.RawUI.ForegroundColor = "Yellow"
+            $host.ui.RawUI.BackgroundColor = "Black"
+
+            Out-Host -InputObject "$logString"
+    
+            $host.ui.RawUI.ForegroundColor = $foregroundColor
+            $host.UI.RawUI.BackgroundColor = $backgroundColor
+
+        }
+    }
+}
+
+
 function Get-TDXAuth($beid, $key) {
 	
     $uri = $apiBaseUri + "auth/loginadmin"
@@ -10,8 +90,10 @@ function Get-TDXAuth($beid, $key) {
         Invoke-RestMethod -Method Post -Uri $uri -Body $body -ContentType "application/json"
     }
     catch {
-        # Display errors and exit script.
-        Write-host "API authentication failed:"
+        Write-Log -level ERROR -string "API authentication failed, see the following log messages for more details."
+        Write-Log -level ERROR -string ("Status Code - " + $_.Exception.Response.StatusCode.value__)
+        Write-Log -level ERROR -string ("Status Description - " + $_.Exception.Response.StatusDescription)
+        Write-Log -level ERROR -string ("Error Message - " + $_.ErrorDetails.Message)
         Exit(1)
     }
 
@@ -37,7 +119,7 @@ function Search-TDXAsset($serialNumber) {
     
     $uri = $apiBaseUri + "$($appID)/assets/search"
     
-    # Filtering options
+    # Using the serial number to filter. More options can be added later
     # https://api.teamdynamix.com/TDWebApi/Home/type/TeamDynamix.Api.Assets.AssetSearch
     $Body = [PSCustomObject]@{
         SerialLike = $serialNumber;
@@ -54,27 +136,30 @@ function Search-TDXAsset($serialNumber) {
 
             # Get the amount of time we need to wait to retry in milliseconds.
             $resetWaitInMs = GetRateLimitWaitPeriodMs -apiCallResponse $_.Exception.Response
-            Write-host "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
+            Write-Log -level WARN -string "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
 
             Start-Sleep -Milliseconds $resetWaitInMs
 
-            Write-host "Retrying API call to retrieve all location custom attribute data for the organization."
+            Write-Log -level WARN -string "Retrying API call"
             Search-Asset -serialNumber $serialNumber
         }
         else {
             # Display errors and exit script.
+            Write-Log -level ERROR -string "Searching for assets failed, see the following log messages for more details."
+            Write-Log -level ERROR -string ("Status Code - " + $_.Exception.Response.StatusCode.value__)
+            Write-Log -level ERROR -string ("Status Description - " + $_.Exception.Response.StatusDescription)
+            Write-Log -level ERROR -string ("Error Message - " + $_.ErrorDetails.Message)
             Exit(1)
         }
     }
 }
 
-function Get-TDXAssetDetails($ID) {
+function Get-TDXAssetAttributes($ID) {
     # Useful for getting atrributes and attachments
     $uri = $apiBaseUri + "$($appID)/assets/$($ID)"
 
     try {
-        $response = Invoke-RestMethod -Method GET -Headers $apiHeaders -Uri $uri -ContentType "application/json" -UseBasicParsing
-        return $response
+        return (Invoke-RestMethod -Method GET -Headers $apiHeaders -Uri $uri -ContentType "application/json" -UseBasicParsing).Attributes
     }
     catch {
         # If we got rate limited, try again after waiting for the reset period to pass.
@@ -83,15 +168,19 @@ function Get-TDXAssetDetails($ID) {
 
             # Get the amount of time we need to wait to retry in milliseconds.
             $resetWaitInMs = GetRateLimitWaitPeriodMs -apiCallResponse $_.Exception.Response
-            Write-host "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
+            Write-Log -level WARN -string "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
 
             Start-Sleep -Milliseconds $resetWaitInMs
 
-            Write-host "Retrying API call to retrieve all location custom attribute data for the organization."
-            Get-TDXAssetDetails -ID $ID
+            Write-Log -level WARN -string "Retrying API call to retrieve all location custom attribute data for the organization."
+            Get-TDXAssetAttributes -ID $ID
         }
         else {
             # Display errors and exit script.
+            Write-Log -level ERROR -string "Getting details on TDX ID $ID has failed, see the following log messages for more details."
+            Write-Log -level ERROR -string ("Status Code - " + $_.Exception.Response.StatusCode.value__)
+            Write-Log -level ERROR -string ("Status Description - " + $_.Exception.Response.StatusDescription)
+            Write-Log -level ERROR -string ("Error Message - " + $_.ErrorDetails.Message)
             Exit(1)
         }
     }
@@ -124,7 +213,7 @@ function Edit-TDXAsset {
     
     $allAttributes = @()
 
-    $assetAttributes = (Get-TDXAssetDetails -ID $asset.ID).Attributes
+    $assetAttributes = Get-TDXAssetAttributes -ID $asset.ID
     foreach ($attribute in $assetAttributes) {
         switch ($attribute.ID) {
             126172 {
@@ -145,7 +234,7 @@ function Edit-TDXAsset {
             }
         }      
     }
-    # Asset properties.
+    # TDX is all or nothing, so gotta upload everything
     # https://pima.teamdynamix.com/SBTDWebApi/Home/type/TeamDynamix.Api.Assets.Asset#properties
     $assetBody = [PSCustomObject]@{
         FormID                  = $Asset.FormID;
@@ -172,9 +261,35 @@ function Edit-TDXAsset {
     } | ConvertTo-Json
     
     $uri = $apiBaseUri + "$($appID)/assets/$($Asset.ID)"
-    $response = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $uri -Body $assetBody -ContentType "application/json" -UseBasicParsing
-    # Create logic to verify edit
-    #return $response
+
+    try {
+        # Wishlist: Create logic to verify edit
+        $response = Invoke-RestMethod -Method POST -Headers $apiHeaders -Uri $uri -Body $assetBody -ContentType "application/json" -UseBasicParsing
+        
+    }
+    catch {
+        # If we got rate limited, try again after waiting for the reset period to pass.
+        $statusCode = $_.Exception.Response.StatusCode.value__
+        if ($statusCode -eq 429) {
+
+            # Get the amount of time we need to wait to retry in milliseconds.
+            $resetWaitInMs = GetRateLimitWaitPeriodMs -apiCallResponse $_.Exception.Response
+            Write-Log -level WARN -string "Waiting $(($resetWaitInMs / 1000.0).ToString("N2")) seconds to rety API call due to rate-limiting."
+
+            Start-Sleep -Milliseconds $resetWaitInMs
+
+            Write-Log -level WARN -string "Retrying API call to retrieve all location custom attribute data for the organization."
+            Get-TDXAssetAttributes -ID $ID
+        }
+        else {
+            # Display errors and exit script.
+            Write-Log -level ERROR -string "Editing the asset PCC Number $($Asset.Tag) has failed, see the following log messages for more details."
+            Write-Log -level ERROR -string ("Status Code - " + $_.Exception.Response.StatusCode.value__)
+            Write-Log -level ERROR -string ("Status Description - " + $_.Exception.Response.StatusDescription)
+            Write-Log -level ERROR -string ("Error Message - " + $_.ErrorDetails.Message)
+            Exit(1)
+        }
+    }
 }
 
 $TDXCreds = Get-Content $PSScriptRoot\TDXCreds.json | ConvertFrom-Json
