233b2dfec72406ba880b2c1a181f084351976d35
Craby4GitHub <github@willcrabtree.com>
2021-09-19 18:36:00 -0700

    Updated comments. Updated edit-asset call.

--- TDX/Update-TDXAssetInventoryDate.ps1
+++ TDX/Update-TDXAssetInventoryDate.ps1
@@ -12,21 +12,25 @@ if ($null -eq (Get-PSDrive -Name 'PCC' -PSProvider CMSite -ErrorAction SilentlyC
 }
 
 # Set the current location to be the site code.
-Set-Location "$($SiteCode):\" #@initParams
+Set-Location "PCC:\" #@initParams
 
+# Load TDX API functions
 . (Join-Path $PSSCRIPTROOT "TDX-API.ps1")
 
-# Pulling all devices from SCCM that have the agent installed
+# Pulling all devices from the SCCM. Once for heartbeat and another for serial numbers 
+# Wishlist: Logic to filter by computer name? Reason: filter out VM's, servers(?)
 $allSccmDevices = Get-CMDevice -CollectionName 'Agent Installed' | Select-Object Name, ResourceID, LastDDR
 $allSccmSerialNumber = Get-WmiObject -Class SMS_G_system_SYSTEM_ENCLOSURE  -Namespace root\sms\site_PCC -ComputerName "do-sccm.pcc-domain.pima.edu" | Select-Object ResourceID, SerialNumber
 Write-Log -level INFO -string "Loaded $($allSccmDevices.count) devices from SCCM"
 
 # Pulling all TDX assets
 # Wishlist: Filter for only computers. Currently also pulls printers, TVs, ect
-$allTDXAssets = Search-TDXAsset
+$allTDXAssets = Search-TDXAssets
 Write-Log -level INFO -string "Loaded $($allTDXAssets.count) devices from TDX"
 
 foreach ($tdxAsset in $allTDXAssets) {
+    [int]$pct = ($allTDXAssets.IndexOf($tdxAsset)/$allTDXAssets.Count)*100
+    Write-progress -Activity '...' -PercentComplete $pct -status "$pct% Complete"
     Write-Log -level INFO -string "Searching for $($tdxAsset.tag) in SCCM records"
 
     # Getting PCC and Serial number for current device
@@ -34,15 +38,16 @@ foreach ($tdxAsset in $allTDXAssets) {
     $sccmDeviceInfo = $allSccmDevices | Where-Object -Property name -like "*$($tdxAsset.Tag)*"
     $sccmDeviceSerialNumber = ($allSccmSerialNumber | Where-Object -Property ResourceID -EQ $sccmDeviceInfo.ResourceID).SerialNumber
     
-    # Verify the assets serial number match. Reason: Computer could be misnamed or there could be virtual machines as we only search SCCM on pcc number
+    # Verifying TDX Serial Number data to SCCM data. Reason: SCCM data is search based on pcc number. A computer could be misnamed or there could be virtual machines
     if ($tdxAsset.SerialNumber -eq $sccmDeviceSerialNumber) {
         Write-Log -level INFO -string "Serial Numbers match"
         
         # Check to see if TDX inventory date is atleast X days older than the last SCCM heartbeat
-        $tdxAssetInventoryDate = Get-date (Get-TDXAssetAttributes -ID $tdxAsset.ID | Where-Object -Property Name -eq 'Last Inventory Date').Value -ErrorAction SilentlyContinue # erroraction for null dates
-        if (($sccmDeviceInfo.LastDDR - $tdxAssetInventoryDate).Days -gt 1) {
+        #$tdxAssetInventoryDate = Get-date (Get-TDXAssetAttributes -ID $tdxAsset.ID | Where-Object -Property Name -eq 'Last Inventory Date').Value -ErrorAction SilentlyContinue # erroraction for null dates
+        if (($sccmDeviceInfo.LastDDR - $tdxAsset.'Last Inventory Date').Days -gt 1) {
             Write-Log -level INFO -string "Updating $($tdxAsset.tag) inventory date to $($sccmDeviceInfo.LastDDR)"
-            Edit-TDXAsset -Asset $tdxAsset -sccmLastHardwareScan $sccmDeviceInfo.LastDDR
+            $tdxAsset.'Last Inventory Date'.Value = $sccmDeviceInfo.LastDDR.ToString("o")
+            Edit-TDXAsset -Asset $tdxAsset #-editName 'Last Inventory Date' -editValue $sccmDeviceInfo.LastDDR.ToString("o")
         }
         else {
             Write-Log -level INFO -string "TDX inventory date is most recent"
