3f3f91c6d08dfb82c824ce079d1df604bd0628ee
Craby4GitHub <github@willcrabtree.com>
2021-09-19 18:37:28 -0700

    Switched to global headers for api calls.

--- JAMF/JAMF-API.ps1
+++ JAMF/JAMF-API.ps1
@@ -1,86 +1,72 @@
 function Get-JamfAuthClassic {
-    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-    $headers.Add("Authorization", "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($Creds.UserName):$($Creds.GetNetworkCredential().Password)")))
-    return $headers
+    $classicHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $classicHeader.Add("Authorization", "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($jamfCredentials.UserName):$($jamfCredentials.GetNetworkCredential().Password)")))
+    return $classicHeader
 }
 function Get-JamfAuthPro {
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-    $headers.Add("Authorization", "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($Creds.UserName):$($Creds.GetNetworkCredential().Password)")))
+    $headers.Add("Authorization", "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($jamfCredentials.UserName):$($jamfCredentials.GetNetworkCredential().Password)")))
     $token = Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v1/auth/token" -Method 'POST' -Headers $Headers -ContentType application/json
-    $BearerHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-    $BearerHeader.Add("Authorization", "Bearer " + $token.token)
-    return $BearerHeader
+    $proHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $proHeader.Add("Authorization", "Bearer " + $token.token)
+    return $proHeader
 }
 
 function Get-JamfComputers {
-    $Headers = Get-JamfAuthClassic
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/computers" -Method 'GET' -Headers $Headers -ContentType application/json).computers.computer.Name
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/computers" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).computers.computer.Name
 }
 
 function Get-JamfMobileDeviceEnrollmentProfiles {
-    $Headers = Get-JamfAuthClassic
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledeviceenrollmentprofiles" -Method 'GET' -Headers $Headers -ContentType application/json).mobile_device_enrollment_profiles.mobile_device_enrollment_profile
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledeviceenrollmentprofiles" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).mobile_device_enrollment_profiles.mobile_device_enrollment_profile
 }
 
 function Get-JamfLicensedSoftware {
-    $Headers = Get-JamfAuthClassic
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware" -Method 'GET' -Headers $Headers -ContentType application/json).licensed_software.licensed_software
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).licensed_software.licensed_software
 }
 
 function Find-JamfLicensedSoftware($SearchTerm) {
     # Find by name or ID
-    $Headers = Get-JamfAuthClassic
-
     if ($SearchTerm -match '^\d{1,}$') {
-        return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware/id/$SearchTerm" -Method 'GET' -Headers $Headers -ContentType application/json).licensed_software.licenses.license
+        return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware/id/$SearchTerm" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).licensed_software.licenses.license
     }
     else {
-        return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware/name/$SearchTerm" -Method 'GET' -Headers $Headers -ContentType application/json).licensed_software.licenses.license
+        return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/licensedsoftware/name/$SearchTerm" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).licensed_software.licenses.license
     }
 }
 
 function Remove-JamfComputer($ID) {
     # You can DELETE using the resource URLs with parameters of {name}, {udid}, {serial number}, or {macaddress}.
-    $Headers = Get-JamfAuthClassic
-    Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/computers/id/$ID" -Method 'DELETE' -Headers $Headers -ContentType application/json
+    Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/computers/id/$ID" -Method 'DELETE' -Headers $jamfClassicHeader -ContentType application/json
 }
 
 function Get-JamfMobileGroups {
-    $Headers = Get-JamfAuthClassic
-    (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups" -Method 'GET' -Headers $Headers -ContentType application/json).mobile_device_groups.mobile_device_group
+    (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).mobile_device_groups.mobile_device_group
 }
 
 function Get-JamfMobileGroupsByID($ID) {
-    $Headers = Get-JamfAuthClassic
-    (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups/id/$ID" -Method 'GET' -Headers $Headers -ContentType application/json).mobile_device_group
+    (Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups/id/$ID" -Method 'GET' -Headers $jamfClassicHeader -ContentType application/json).mobile_device_group
 }
 
 function Update-JamfMobileGroups($ID, [array]$SerialNumbers) {
-    $Headers = Get-JamfAuthClassic
-
     $body = @"
     <mobile_device_additions>key</mobile_device_additions>
 "@
-    Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups/id/$ID" -Method 'PUT' -Headers $Headers -Body $body -ContentType application/xml
+    Invoke-RestMethod "https://pccjamf.jamfcloud.com/JSSResource/mobiledevicegroups/id/$ID" -Method 'PUT' -Headers $jamfClassicHeader -Body $body -ContentType application/xml
 }
 
 function Get-JamfMobileDevicePreStage {
-    $auth = Get-JamfAuthPro
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages?page=0&page-size=1000&sort=id%3Adesc" -Method 'GET' -Headers $auth -ContentType application/json).results
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages?page=0&page-size=1000&sort=id%3Adesc" -Method 'GET' -Headers $jamfProHeader -ContentType application/json).results
 }
 
 function Get-JamfMobileDevicePreStageByID($ID) {
-    $auth = Get-JamfAuthPro
-    return Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID" -Method 'GET' -Headers $auth -ContentType application/json
+    return Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID" -Method 'GET' -Headers $jamfProHeader -ContentType application/json
 }
 function Get-JamfMobileDevicePreStageScopeByID($ID) {
-    $auth = Get-JamfAuthPro
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'GET' -Headers $auth -ContentType application/json).assignments.serialnumber
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'GET' -Headers $jamfProHeader -ContentType application/json).assignments.serialnumber
 }
 
 function Get-JamfMobileDevicePreStageScope {
-    $auth = Get-JamfAuthPro
-    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/scope" -Method 'GET' -Headers $auth -ContentType application/json).serialsByPrestageId
+    return (Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/scope" -Method 'GET' -Headers $jamfProHeader -ContentType application/json).serialsByPrestageId
 }
 
 function Update-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
@@ -89,8 +75,7 @@ function Update-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
         "serialNumbers" = $SerialNumbers;
         "versionLock"   = $versionLock;
     } | ConvertTo-Json
-    $auth = Get-JamfAuthPro
-    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'PUT' -Headers $auth -Body $params -ContentType application/json
+    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'PUT' -Headers $jamfProHeader -Body $params -ContentType application/json
 }
 
 function Remove-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
@@ -99,8 +84,7 @@ function Remove-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
         "serialNumbers" = $SerialNumbers;
         "versionLock"   = $versionLock;
     } | ConvertTo-Json
-    $auth = Get-JamfAuthPro
-    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope/delete-multiple" -Method 'POST' -Headers $auth -Body $params -ContentType application/json
+    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope/delete-multiple" -Method 'POST' -Headers $jamfProHeader -Body $params -ContentType application/json
 }
 
 function Add-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
@@ -109,8 +93,10 @@ function Add-JamfMobileDeviceFromPreStageScope($ID, [array]$SerialNumbers) {
         "serialNumbers" = $SerialNumbers;
         "versionLock"   = $versionLock;
     } | ConvertTo-Json
-    $auth = Get-JamfAuthPro
-    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'POST' -Headers $auth -Body $params -ContentType application/json
+    Invoke-RestMethod "https://pccjamf.jamfcloud.com/api/v2/mobile-device-prestages/$ID/scope" -Method 'POST' -Headers $jamfProHeader -Body $params -ContentType application/json
 }
 
-$Creds = Get-Credential
+$jamfCredentials = Get-Credential
+
+$global:jamfClassicHeader = Get-JamfAuthClassic
+$global:jamfProHeader = Get-JamfAuthPro
