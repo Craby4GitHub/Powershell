6d5407c34db24d0bc566dd89c7098061f5be0366
Will R Crabtree <github@willcrabtree.com>
2022-07-15 16:49:19 -0700

    Location Filter
    Locations are not used for People, yet.
    
    Updated setting of the responsible group to use WorkAddress.
    
    Added temp filter to allow using script in stages.

--- TDX/Inventory.ps1
+++ TDX/Inventory.ps1
@@ -4,7 +4,7 @@
 $allTDXAssets = Search-TDXAssets
 
 # Find all assets with an Owner that arent disposed
-$assetOwners = $allTDXAssets | Where-Object {($_.OwningCustomerName -NE 'None') -and ($_.StatusName -ne 'Disposed')}
+$assetOwners = $allTDXAssets | Where-Object { ($_.OwningCustomerName -NE 'None') -and ($_.StatusName -ne 'Disposed') }
 
 # Set up an empty array for users already worked on
 $coveredUser = New-Object System.Collections.Generic.List[System.Object]
@@ -38,7 +38,7 @@ foreach ($assetOwner in $assetOwners) {
             # Get the last inventory date
             $lastInventoryDate = $assetCustomAttributes | Where-Object -Property Name -eq 'Last Inventory Date' | Select-Object -ExpandProperty value 
 
-            # 
+            # Check to see if the current inventory date is older than the oldest allowed inventory date
             if ($inventoryDateCutOff -gt $lastInventoryDate) {
                 $assetNotInventoriedRecently = $true
                 break :inner
@@ -46,19 +46,26 @@ foreach ($assetOwner in $assetOwners) {
         }
 
         if ($assetNotInventoriedRecently) {
-            # Prepare ticket description
-            $Description = "Hello $($assetOwner.OwningCustomerName),`nProperty Control is performing an annual inventory of IT assets. Please review the assets below and reply to this email/ticket indicating if the asset information is correct. If incorrect, please list any discrepancies.`nIf you have other IT assets that are not listed below, please provide the PCC number and a brief description of the device in your response. We will then update our records.`n`n"
-
-            # Adding users assets to Description
-            foreach ($userAsset in $allUserAssets) {
-                $assetCustomAttributes = Get-TDXAssetAttributes -ID $userAsset.ID
-                $Description += "PCC Number: $($userAsset.Tag)`nAsset Model: $($userAsset.ManufacturerName) $($userAsset.ProductModelName)`nCampus and Room: $($userAsset.LocationName) $($userAsset.LocationRoomName)`n`n" 
-            }
-
-            $Description += "`nIf you have any questions, please feel free to respond to this email or call our Service Desk at 206-4900 and reference this ticket."
-
-            # Get the users home location and set the responsible group based on that
+            
+            # Get user details
             $userDetails = Get-TDXPersonDetails -UID $assetOwner.OwningCustomerID
+
+            if ($userDetails.WorkAddress.Split(' ')[0] -like '29') {
+                write-host '29th!'
+            
+                # Prepare ticket description
+                $Description = "Hello $($assetOwner.OwningCustomerName),`nProperty Control is performing an annual inventory of IT assets. Please review the assets below and reply to this email/ticket indicating if the asset information is correct. If incorrect, please list any discrepancies.`nIf you have other IT assets that are not listed below, please provide the PCC number and a brief description of the device in your response. We will then update our records.`n`n"
+
+                # Adding users assets to Description
+                foreach ($userAsset in $allUserAssets) {
+                    $assetCustomAttributes = Get-TDXAssetAttributes -ID $userAsset.ID
+                    $Description += "PCC Number: $($userAsset.Tag)`nAsset Model: $($userAsset.ManufacturerName) $($userAsset.ProductModelName)`nCampus and Room: $($userAsset.LocationName) $($userAsset.LocationRoomName)`n`n" 
+                }
+
+                $Description += "`nIf you have any questions, please feel free to respond to this email or call our Service Desk at 206-4900 and reference this ticket."
+
+                <#  Cant use LocationName because this isnt loaded from Banner... :[    Save for later if this maybe gets fixed?
+            # Get the users home location and set the responsible group based on that
             switch ($userDetails.LocationName) {
                 "29th St LC" { $responsibleGroup = 11684 } # DO
                 "Ajo" { $responsibleGroup = 11684 } # DO
@@ -81,29 +88,46 @@ foreach ($assetOwner in $assetOwners) {
                 "West Campus" { $responsibleGroup = 11679 } # WC
                 Default { $responsibleGroup = 11412 } # SD
             }
+            #>
+                # Get the users home location based on the work address and set the responsible group based on that
+                switch ($userDetails.WorkAddress.Split(' ')[0]) {
+                    "29" { $responsibleGroup = 11684 } # DO
+                    "DV" { $responsibleGroup = 11682 } # DV
+                    "DO" { $responsibleGroup = 11684 } # DO
+                    "DC" { $responsibleGroup = 11681 } # DC
+                    "East" { $responsibleGroup = 11683 } # EC
+                    "EC" { $responsibleGroup = 11683 } # EC
+                    "El" { $responsibleGroup = 11684 } # DO
+                    "MS" { $responsibleGroup = 11684 } # DO
+                    "NW" { $responsibleGroup = 11680 } # NW
+                    "Santa" { $responsibleGroup = 11682 } # DV
+                    "WC" { $responsibleGroup = 11679 } # WC
+                    Default { $responsibleGroup = 11412 } # SD
+                }
 
-            # Setup the ticket attributes
-            $ticketOptions = @{
-                AccountID          = 75673 # Campus Staff(CAMSTF)
-                PriorityID         = 4537 # Normal
-                RequestorUid       = $assetOwner.OwningCustomerID 
-                ResponsibleGroupID = $responsibleGroup # Based on the users home site
-                StatusID           = 52423 # In Process
-                Title              = 'IT Asset Inventory 2022' 
-                TypeID             = 30386 # USS - Service Desk
-                ClassificationID   = '46' # Service Request
-                Description        = $Description 
-                ServiceID          = 43361 # Campus IT Maintenance Tasks
-                #FormID             = 57683 # Asset Service
-                LocationID         = $userDetails.LocationID
-            }
+                # Setup the ticket attributes
+                $ticketOptions = @{
+                    AccountID          = 75673 # Campus Staff(CAMSTF)
+                    PriorityID         = 4537 # Normal
+                    RequestorUid       = $assetOwner.OwningCustomerID 
+                    ResponsibleGroupID = $responsibleGroup # Based on the users home site
+                    StatusID           = 52423 # In Process
+                    Title              = 'IT Asset Inventory 2022' 
+                    TypeID             = 30386 # USS - Service Desk
+                    ClassificationID   = '46' # Service Request
+                    Description        = $Description 
+                    ServiceID          = 43361 # Campus IT Maintenance Tasks
+                    #FormID             = 57683 # Asset Service
+                    LocationID         = $userDetails.LocationID
+                }
 
-            # Create the ticket
-            $ticket = Submit-TDXTicket @ticketOptions
+                # Create the ticket
+                $ticket = Submit-TDXTicket @ticketOptions
 
-            # Add assets directly to the ticket
-            foreach ($userAsset in $allUserAssets) {
-                Edit-TDXTicketAddAsset -ticketID $ticket.ID -assetID $userAsset.ID        
+                # Add assets directly to the ticket
+                foreach ($userAsset in $allUserAssets) {
+                    Edit-TDXTicketAddAsset -ticketID $ticket.ID -assetID $userAsset.ID        
+                }
             }
         }
     }
