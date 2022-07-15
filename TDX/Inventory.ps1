b0f35e1d09603ab6e115ba2987eb5fdde20ad6b8
Will R Crabtree <github@willcrabtree.com>
2022-07-15 12:15:03 -0700

    Inventory date filter and updates
    Instead of creating a ticket for every Owner, only for those with an asset not recently inventoried.
    
    Added filter for disposed assets.
    
    Updated ticket description.

--- TDX/Inventory.ps1
+++ TDX/Inventory.ps1
@@ -3,18 +3,21 @@
 # Get all TDX Assets
 $allTDXAssets = Search-TDXAssets
 
-# Find all assets with an Owner
-$assetOwners = $allTDXAssets | Where-Object -Property OwningCustomerName -NE 'None'
+# Find all assets with an Owner that arent disposed
+$assetOwners = $allTDXAssets | Where-Object {($_.OwningCustomerName -NE 'None') -and ($_.StatusName -ne 'Disposed')}
 
 # Set up an empty array for users already worked on
 $coveredUser = New-Object System.Collections.Generic.List[System.Object]
+
+# Set oldest allowed inventory date
+$inventoryDateCutOff = Get-Date '04/30/2021'
 
 # Go through each Asset Owner
 foreach ($assetOwner in $assetOwners) {
     [int]$pct = ($assetOwners.IndexOf($assetOwner) / $assetOwners.count) * 100
     Write-progress -Activity "Working on $($assetOwner.OwningCustomerName)" -percentcomplete $pct -status "$pct% Complete"
 
-    # See if user already worked on and skip them if so
+    # See if user has been already worked on and skip them if so
     if ($coveredUser.IndexOf($assetOwner.OwningCustomerName) -ge 0) {
         # Skip
     }
@@ -25,61 +28,83 @@ foreach ($assetOwner in $assetOwners) {
         # Get all assets assigned to user
         $allUserAssets = $assetOwners | Where-Object -Property OwningCustomerName -EQ $assetOwner.OwningCustomerName
 
-        # Prepare ticket description
-        $Description = "Hello $($assetOwner.OwningCustomerName),`nLink to your Profile: https://service.pima.edu/TDClient/1920/Portal/People/Details?ID=$($assetOwner.OwningCustomerID)`nThe following assets are assigned to you:`n`n"
+        # Get the custom attributes for each asset to verify Last Inventory Date is within X months
+        $assetNotInventoriedRecently = $false
+        :inner foreach ($userAsset in $allUserAssets) {
 
-        # Adding users assets to Description
-        foreach ($userAsset in $allUserAssets) {
-            $Description += "PCC Number: $($userAsset.Tag)`nAsset Model: $($userAsset.ManufacturerName) $($userAsset.ProductModelName)`nCampus and Room: $($userAsset.LocationName) $($userAsset.LocationRoomName)`n`n" 
+            # Get custom attributes
+            $assetCustomAttributes = Get-TDXAssetAttributes -ID $userAsset.ID
+
+            # Get the last inventory date
+            $lastInventoryDate = $assetCustomAttributes | Where-Object -Property Name -eq 'Last Inventory Date' | Select-Object -ExpandProperty value 
+
+            # 
+            if ($inventoryDateCutOff -gt $lastInventoryDate) {
+                $assetNotInventoriedRecently = $true
+                break :inner
+            }
         }
 
-        # Get the users home location and set the responsible group based on that
-        $userDetails = Get-TDXPersonDetails -UID $assetOwner.OwningCustomerID
-        switch ($userDetails.LocationName) {
-            "29th St LC" { $responsibleGroup = 11412 } # SD
-            "Ajo" { $responsibleGroup = 11412 } # SD
-            "Arizona State Prison - Douglas" { $responsibleGroup = 11683 } # EC
-            "Arizona State Prison - Wilmot" { $responsibleGroup = 11683 } # EC
-            "Aviation Technology" { $responsibleGroup = 11682 } # DV
-            "Davis Monthan AFB" { $responsibleGroup = 11683 } # EC
-            "Desert Vista Campus" { $responsibleGroup = 11682 } # DV
-            "District Office" { $responsibleGroup = 11684 } # DO
-            "Downtown Campus" { $responsibleGroup = 11681 } # DC
-            "East Campus" { $responsibleGroup = 11683 } # EC
-            "El Pueblo LC" { $responsibleGroup = 11412 } # SD
-            "El Rio LC" { $responsibleGroup = 11412 } # SD
-            "Maintenance and Security" { $responsibleGroup = 11684 } # DO
-            "Northwest Campus" { $responsibleGroup = 11680 } # NW
-            "Not Campus Specific" { $responsibleGroup = 11412 } # SD
-            "Pima County Jail" { $responsibleGroup = 11412 } # SD
-            "Santa Cruz Center" { $responsibleGroup = 11412 } # SD
-            "Truck Driver Training" { $responsibleGroup = 11684 } # DO
-            "West Campus" { $responsibleGroup = 11679 } # WC
-            Default { $responsibleGroup = 11412 } # SD
-        }
+        if ($assetNotInventoriedRecently) {
+            # Prepare ticket description
+            $Description = "Hello $($assetOwner.OwningCustomerName),`nProperty Control is performing an annual inventory of IT assets. Please review the assets below and reply to this email/ticket indicating if the asset information is correct. If incorrect, please list any discrepancies.`nIf you have other IT assets that are not listed below, please provide the PCC number and a brief description of the device in your response. We will then update our records.`n`n"
 
-        # Setup the ticket attributes
-        $ticketOptions = @{
-            AccountID          = 75673 # Campus Staff(CAMSTF)
-            PriorityID         = 4537 # Normal
-            RequestorUid       = $assetOwner.OwningCustomerID 
-            ResponsibleGroupID = $responsibleGroup # Based on the users home site
-            StatusID           = 52423 # In Process
-            Title              = 'IT Asset Inventory 2022' 
-            TypeID             = 41029 # Asset Management
-            ClassificationID = '46' # Service Request
-            Description        = $Description 
-            ServiceID          = 51998 # Asset Management
-            #FormID             = 57683 # Asset Service
-            LocationID        = $userDetails.LocationID
-        }
+            # Adding users assets to Description
+            foreach ($userAsset in $allUserAssets) {
+                $assetCustomAttributes = Get-TDXAssetAttributes -ID $userAsset.ID
+                $Description += "PCC Number: $($userAsset.Tag)`nAsset Model: $($userAsset.ManufacturerName) $($userAsset.ProductModelName)`nCampus and Room: $($userAsset.LocationName) $($userAsset.LocationRoomName)`n`n" 
+            }
 
-        # Create the ticket
-        $ticket = Submit-TDXTicket @ticketOptions
+            $Description += "`nIf you have any questions, please feel free to respond to this email or call our Service Desk at 206-4900 and reference this ticket."
 
-        # Add assets directly to the ticket
-        foreach ($userAsset in $allUserAssets) {
-            Edit-TDXTicketAddAsset -ticketID $ticket.ID -assetID $userAsset.ID        
+            # Get the users home location and set the responsible group based on that
+            $userDetails = Get-TDXPersonDetails -UID $assetOwner.OwningCustomerID
+            switch ($userDetails.LocationName) {
+                "29th St LC" { $responsibleGroup = 11684 } # DO
+                "Ajo" { $responsibleGroup = 11684 } # DO
+                "Arizona State Prison - Douglas" { $responsibleGroup = 11683 } # EC
+                "Arizona State Prison - Wilmot" { $responsibleGroup = 11683 } # EC
+                "Aviation Technology" { $responsibleGroup = 11682 } # DV
+                "Davis Monthan AFB" { $responsibleGroup = 11683 } # EC
+                "Desert Vista Campus" { $responsibleGroup = 11682 } # DV
+                "District Office" { $responsibleGroup = 11684 } # DO
+                "Downtown Campus" { $responsibleGroup = 11681 } # DC
+                "East Campus" { $responsibleGroup = 11683 } # EC
+                "El Pueblo LC" { $responsibleGroup = 11684 } # DO
+                "El Rio LC" { $responsibleGroup = 11684 } # DO
+                "Maintenance and Security" { $responsibleGroup = 11684 } # DO
+                "Northwest Campus" { $responsibleGroup = 11680 } # NW
+                "Not Campus Specific" { $responsibleGroup = 11684 } # DO
+                "Pima County Jail" { $responsibleGroup = 11684 } # DO
+                "Santa Cruz Center" { $responsibleGroup = 11682 } # DV
+                "Truck Driver Training" { $responsibleGroup = 11684 } # DO
+                "West Campus" { $responsibleGroup = 11679 } # WC
+                Default { $responsibleGroup = 11412 } # SD
+            }
+
+            # Setup the ticket attributes
+            $ticketOptions = @{
+                AccountID          = 75673 # Campus Staff(CAMSTF)
+                PriorityID         = 4537 # Normal
+                RequestorUid       = $assetOwner.OwningCustomerID 
+                ResponsibleGroupID = $responsibleGroup # Based on the users home site
+                StatusID           = 52423 # In Process
+                Title              = 'IT Asset Inventory 2022' 
+                TypeID             = 30386 # USS - Service Desk
+                ClassificationID   = '46' # Service Request
+                Description        = $Description 
+                ServiceID          = 43361 # Campus IT Maintenance Tasks
+                #FormID             = 57683 # Asset Service
+                LocationID         = $userDetails.LocationID
+            }
+
+            # Create the ticket
+            $ticket = Submit-TDXTicket @ticketOptions
+
+            # Add assets directly to the ticket
+            foreach ($userAsset in $allUserAssets) {
+                Edit-TDXTicketAddAsset -ticketID $ticket.ID -assetID $userAsset.ID        
+            }
         }
     }
 }
