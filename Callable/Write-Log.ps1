9f2770377a41746c077ddcf6cc24d65cad57d6c2
Craby4GitHub <github@willcrabtree.com>
2022-07-15 20:50:26 -0700

    Update Write-Log.ps1
    Removed old parameter.

--- Callable/Write-Log.ps1
+++ Callable/Write-Log.ps1
@@ -7,12 +7,10 @@ function Write-Log {
 
         [Parameter(Mandatory = $true)]
         [string]$message,
-
-        $assetSerialNumber
     )
     	
     $timeStamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-    $logString = "$timeStamp, $level, $assetSerialNumber, $message"
+    $logString = "$timeStamp, $level, $message"
 
     Add-Content -Path $logFile -Value $logString -Force
 
