01f88c5a11cdd30967d6c3c8205df6f5ff7baa63
Craby4GitHub <github@willcrabtree.com>
2022-11-18 09:02:35 -0700

    Inactive Student rework
    Now will set the requestor of the ticket to the user provided information instead of the generic Inactive Student account.

--- Automation/BB-TDX Ticket Import.ps1
+++ Automation/BB-TDX Ticket Import.ps1
@@ -82,32 +82,19 @@ if ($authResponse.error_msg -eq 'login success') {
             $caseDetailsConverted = Get-Content 'c:\users\wrcrabtree\desktop\casedetail.txt'
             $caseDetailsConverted = $caseDetailsConverted -replace '&nbsp;', ' '
             $caseDetailsConverted = $caseDetailsConverted -replace '<[^>]+>', ''
-            # Get user TDX user profile to have them as the requestor in the ticket
-            $tdxUserUID = '05a7e4ad-3409-ed11-bd6e-0003ff5063cd' # Inactive Student
-            if ($userANumber -match 'a\d{8}') {
-                $tdxUser = Search-TDXPeople -SearchString $userANumber
-                if ($null -ne $tdxUser.UID) {
-                    $tdxUserUID = $tdxUser.UID
-                }
-                else {
-                    $tdxUserUID = '05a7e4ad-3409-ed11-bd6e-0003ff5063cd' # Inactive Student
-                }
-            }
 
             # Setup the ticket attributes based on TDX application
             switch ($queue.TDXAppName) {
                 ITTicket {
+
                     $Description = "Hello $($request.customer.name.split(' ')[0]),`n
-                    The IT Service Desk at Pima Community College has received your case from our Tier 1 support. We will be contacting you soon via the contact information listed below.`n
-                        Case Details: $($caseDetailsConverted)
-                        Phone Number: $userPhoneNumber
-                        Email: $userEmail `n
+                    The IT Service Desk at Pima Community College has received your case from our Tier 1 support and we will be contacting you shortly.`n`n
+                        Case Details: $($caseDetailsConverted)`n`n
                         If this is an urgent matter, please call us at (520) 206-4900 and reference this ticket.`n
                         "
                     $ticketAttributes = @{
                         AccountID          = 94478 # Students(PCCSTU)
                         PriorityID         = 4537 # Normal
-                        RequestorUid       = $tdxUserUID 
                         ResponsibleGroupID = 11412 # USS - Service Desk
                         StatusID           = 52421 # New
                         Title              = $ticket.summary 
@@ -128,68 +115,83 @@ if ($authResponse.error_msg -eq 'login success') {
                             }
                         )
                     }
+
+                    # Get user TDX user profile to have them as the requestor in the ticket
+                    $tdxUser = Search-TDXPeople -SearchString $userANumber
+                    if ($null -ne $tdxUser.UID) {
+                        $ticketAttributes.RequestorUid = $tdxUser.UID 
+                    }
+                    else {
+                        $ticketAttributes.RequestorUid = $null
+                        $ticketAttributes.RequestorName = $request.customer.name                         # The full name of the requestor associated with the ticket.
+                        $ticketAttributes.RequestorFirstName = "$($request.customer.name.split(' ')[0])" # The first name of the requestor associated with the ticket.
+                        $ticketAttributes.RequestorLastName = "$($request.customer.name.split(' ')[1])"	 # The last name of the requestor associated with the ticket.
+                        $ticketAttributes.RequestorEmail = $userEmail		                             # The email address of the requestor associated with the ticket.
+                        $ticketAttributes.RequestorPhone = $userPhoneNumber	                             # The phone number of the requestor associated with the ticket.
+                    }
                 }
-                D2LTicket {
-                    $Description = "Hello $($request.customer.name.split(' ')[0]),`n
+            }
+            D2LTicket {
+                $Description = "Hello $($request.customer.name.split(' ')[0]),`n
                     The D2L Support team at Pima Community College has received your case from our Tier 1 support. We will be contacting you soon via the contact information listed below.`n
                         Case Details: $($caseDetailsConverted)
                         Phone Number: $userPhoneNumber
                         Email: $userEmail `n
                      "
 
-                    $ticketAttributes = @{
-                        AccountID          = 94478 # Students(PCCSTU)
-                        PriorityID         = 4537 # Normal
-                        RequestorUid       = $tdxUserUID 
-                        ResponsibleGroupID = 14726 # Distance Ed
-                        StatusID           = 89316 # New
-                        Title              = "BlackBoard Ticket: $ticketNumber - $($ticket.summary)"
-                        TypeID             = 33037 # Distance Ed
-                        ClassificationID   = '46' # Service Request
-                        Description        = $Description
-                        ServiceID          = 39394 # D2L/Brightspace Support
-                        FormID             = 43510 # Incident
-                        SourceID           = 2517 # Blackboard
-                        IsRichHtml         = $true
-                        AppName            = 'D2LTicket'
-                    }
-                }
-                Default {
-                    exit(1)
-                    write-host "No TDX app anme"
+                $ticketAttributes = @{
+                    AccountID          = 94478 # Students(PCCSTU)
+                    PriorityID         = 4537 # Normal
+                    RequestorUid       = $tdxUserUID 
+                    ResponsibleGroupID = 14726 # Distance Ed
+                    StatusID           = 89316 # New
+                    Title              = "BlackBoard Ticket: $ticketNumber - $($ticket.summary)"
+                    TypeID             = 33037 # Distance Ed
+                    ClassificationID   = '46' # Service Request
+                    Description        = $Description
+                    ServiceID          = 39394 # D2L/Brightspace Support
+                    FormID             = 43510 # Incident
+                    SourceID           = 2517 # Blackboard
+                    IsRichHtml         = $true
+                    AppName            = 'D2LTicket'
                 }
             }
+            Default {
+                exit(1)
+                write-host "No TDX app anme"
+            }
+        }
                     
-            # Create the ticket
-            $tdxTicket = $null
-            $tdxTicket = Submit-TDXTicket @ticketAttributes
+        # Create the ticket
+        $tdxTicket = $null
+        $tdxTicket = Submit-TDXTicket @ticketAttributes
     
-            # Verify the ticket was created
-            if ($null -ne $tdxTicket.id) {
-                write-host $tdxTicket.id
+        # Verify the ticket was created
+        if ($null -ne $tdxTicket.id) {
+            write-host $tdxTicket.id
 
-                # Add BB ticket history to TDX ticket
-                if ($ticket.history.count -ge 1) {
+            # Add BB ticket history to TDX ticket
+            if ($ticket.history.count -ge 1) {
 
-                    # Reversing history as it would put the newest comment at the bottom
-                    [array]::Reverse($ticket.history)
+                # Reversing history as it would put the newest comment at the bottom
+                [array]::Reverse($ticket.history)
 
-                    foreach ($history in $ticket.history) {
+                foreach ($history in $ticket.history) {
 
-                        # Need to convert email responses as uploading to TDX causes errors.
-                        # This will select the most recent email comment and post it as a comment
-                        if ($history.action_type -eq 'EMAIL_RESPONSE') {
-                            $HTML = New-Object -Com "HTMLFile"
-                            $src = [System.Text.Encoding]::Unicode.GetBytes($history.comment)
-                            $html.write($src)
-                            $history.comment = $html.all.tags("div") | ForEach-Object innertext | Select-Object -first 1
-                        }
+                    # Need to convert email responses as uploading to TDX causes errors.
+                    # This will select the most recent email comment and post it as a comment
+                    if ($history.action_type -eq 'EMAIL_RESPONSE') {
+                        $HTML = New-Object -Com "HTMLFile"
+                        $src = [System.Text.Encoding]::Unicode.GetBytes($history.comment)
+                        $html.write($src)
+                        $history.comment = $html.all.tags("div") | ForEach-Object innertext | Select-Object -first 1
+                    }
 
-                        # converting unix time...
-                        $UnixDate = [int64]$history.action_date / 1000
-                        $time = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($UnixDate))
+                    # converting unix time...
+                    $UnixDate = [int64]$history.action_date / 1000
+                    $time = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($UnixDate))
 
-                        $comment = "Date: $time`n
+                    $comment = "Date: $time`n
 
                         Action: $($history.action)`n
 
@@ -198,78 +200,78 @@ if ($authResponse.error_msg -eq 'login success') {
                         Comment: $($history.comment)"
 
 
-                        $ticketHistory = Update-TDXTicket -ticketID $tdxTicket.ID -Comment $comment -IsPrivate $true -IsRichHtml $true -AppName $queue.TDXAppName
-                    }
+                    $ticketHistory = Update-TDXTicket -ticketID $tdxTicket.ID -Comment $comment -IsPrivate $true -IsRichHtml $true -AppName $queue.TDXAppName
                 }
-
-                # Check if there are chat transcripts and add as a comment to TDX ticket
-                # TODO: have it so it outputs to tdx in a more clean manner
-                if ($ticket.chatTranscriptAttachment.count -ge 1) {
-                    write-host "External chat log found, adding to the ticket" -ForegroundColor Yellow
-                    foreach ($chat in $ticket.chatTranscriptAttachment) {
-                        $chatLog = Invoke-RestMethod -Method GET -Uri "$baseURL/2028/file/$($chat.ID)?token=$token"
-                        
-                        #$chatLogConverted = $chatLog -replace '<[^>]+>', ''
-                        
-                        $tdxChatLog = Update-TDXTicket -ticketID $tdxTicket.ID -Comment $chatLog -IsPrivate $true -IsRichHtml $false -AppName $queue.TDXAppName
-                    }
-                }
-
-                # Check if there are attachments and add to TX ticket
-                # Current: does not attach to the ticket as I dont have the API command setup to add attachments
-                # Ideas: check for .txt | .html and add as commnt to ticket?
-                if ($ticket.Attachment.count -ge 1) {
-                    #$ticketattachment = Update-TDXTicket -ticketID $tdxTicket.ID -Comment "There are attachments related to this ticket. Please navigate to this ticket in Blackboard to download." -IsPrivate $true -IsRichHtml $true -AppName $queue.TDXAppName
-                    foreach ($attachment in $ticket.Attachment) {
-                        write-host "attachments found" -ForegroundColor Yellow
-                        #Invoke-webrequest -Uri "$baseURL/2028/file/$($attachment.ID)?token=$token" -UseBasicParsing -OutFile "c:\users\wrcrabtree\downloads\$($attachment.id)+$($attachment.name)"   
-                    }
-                }
-
-
-                # Create a blank object which will be used for an API that assigns the ticket to us
-                $body = [PSCustomObject]@{
-                    attachment           = @()
-                    comment              = ""
-                    csr                  = ""
-                    csrCCList            = ""
-                    customerCCList       = ""
-                    queue                = ""
-                    selectedCsr          = ""
-                    suppressExtComm      = ""
-                    transferInstitution  = ""
-                    transferTemplateType = ""
-                    useKbs               = @()
-                } | ConvertTo-Json
-
-                $grabTicket = Invoke-RestMethod -Method POST -Uri "$baseURL/2028/case/$($ticketNumber)/action/31745?token=$token" -ErrorVariable apiError -ContentType "application/json" -Body $body
-                write-host $grabTicket
-
-
-                # Create a object with a comment which will be used for an API that closes the ticket
-
-                $body = [PSCustomObject]@{
-                    attachment           = @()
-                    comment              = "Ticket has been escalated to the PCCs IT ticketing system. Ticket: https://service.pima.edu/TDClient/1920/Portal/Requests/TicketRequests/TicketDet?TicketID=$($tdxTicket.id)"
-                    csr                  = ""
-                    csrCCList            = ""
-                    customerCCList       = ""
-                    queue                = ""
-                    selectedCsr          = ""
-                    suppressExtComm      = ""
-                    transferInstitution  = ""
-                    transferTemplateType = ""
-                    useKbs               = @()
-                } | ConvertTo-Json
-
-                $closeTicket = Invoke-RestMethod -Method POST -Uri "$baseURL/2028/case/$($ticketNumber)/action/31734?token=$token" -ErrorVariable apiError -ContentType "application/json" -Body $body
-                write-host $closeTicket
             }
-            else {
-                Write-host "Failed to Create ticket for $ticketNumber"
+
+            # Check if there are chat transcripts and add as a comment to TDX ticket
+            # TODO: have it so it outputs to tdx in a more clean manner
+            if ($ticket.chatTranscriptAttachment.count -ge 1) {
+                write-host "External chat log found, adding to the ticket" -ForegroundColor Yellow
+                foreach ($chat in $ticket.chatTranscriptAttachment) {
+                    $chatLog = Invoke-RestMethod -Method GET -Uri "$baseURL/2028/file/$($chat.ID)?token=$token"
+                        
+                    #$chatLogConverted = $chatLog -replace '<[^>]+>', ''
+                        
+                    $tdxChatLog = Update-TDXTicket -ticketID $tdxTicket.ID -Comment $chatLog -IsPrivate $true -IsRichHtml $false -AppName $queue.TDXAppName
+                }
             }
-        } 
-    }
+
+            # Check if there are attachments and add to TX ticket
+            # Current: does not attach to the ticket as I dont have the API command setup to add attachments
+            # Ideas: check for .txt | .html and add as commnt to ticket?
+            if ($ticket.Attachment.count -ge 1) {
+                #$ticketattachment = Update-TDXTicket -ticketID $tdxTicket.ID -Comment "There are attachments related to this ticket. Please navigate to this ticket in Blackboard to download." -IsPrivate $true -IsRichHtml $true -AppName $queue.TDXAppName
+                foreach ($attachment in $ticket.Attachment) {
+                    write-host "attachments found" -ForegroundColor Yellow
+                    #Invoke-webrequest -Uri "$baseURL/2028/file/$($attachment.ID)?token=$token" -UseBasicParsing -OutFile "c:\users\wrcrabtree\downloads\$($attachment.id)+$($attachment.name)"   
+                }
+            }
+
+
+            # Create a blank object which will be used for an API that assigns the ticket to us
+            $body = [PSCustomObject]@{
+                attachment           = @()
+                comment              = ""
+                csr                  = ""
+                csrCCList            = ""
+                customerCCList       = ""
+                queue                = ""
+                selectedCsr          = ""
+                suppressExtComm      = ""
+                transferInstitution  = ""
+                transferTemplateType = ""
+                useKbs               = @()
+            } | ConvertTo-Json
+
+            $grabTicket = Invoke-RestMethod -Method POST -Uri "$baseURL/2028/case/$($ticketNumber)/action/31745?token=$token" -ErrorVariable apiError -ContentType "application/json" -Body $body
+            write-host $grabTicket
+
+
+            # Create a object with a comment which will be used for an API that closes the ticket
+
+            $body = [PSCustomObject]@{
+                attachment           = @()
+                comment              = "Ticket has been escalated to the PCCs IT ticketing system. Ticket: https://service.pima.edu/TDClient/1920/Portal/Requests/TicketRequests/TicketDet?TicketID=$($tdxTicket.id)"
+                csr                  = ""
+                csrCCList            = ""
+                customerCCList       = ""
+                queue                = ""
+                selectedCsr          = ""
+                suppressExtComm      = ""
+                transferInstitution  = ""
+                transferTemplateType = ""
+                useKbs               = @()
+            } | ConvertTo-Json
+
+            $closeTicket = Invoke-RestMethod -Method POST -Uri "$baseURL/2028/case/$($ticketNumber)/action/31734?token=$token" -ErrorVariable apiError -ContentType "application/json" -Body $body
+            write-host $closeTicket
+        }
+        else {
+            Write-host "Failed to Create ticket for $ticketNumber"
+        }
+    } 
+}
 }
 else {
     Write-Host "BB Auth failed"
